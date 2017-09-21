501a855d72af1e0f546e4a5e1cb1bd61c5f1380e
wangh107 <wangh107@gene.com>
2017-09-20 19:51:06 -0700

    add some utility functions

---
+++ R/kmplot_regular.R
@@ -0,0 +1,8 @@
+
+#' Function to create regular Kaplan Meier Plot
+#' 
+#' This function allows you to create KM plot with comparison between treatment arms, seperate plots by subpopulations,
+#' and also apply stratification analysis
+#' 
+#' 
+
