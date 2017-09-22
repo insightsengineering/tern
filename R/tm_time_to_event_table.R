5084abd93bbea8190dcb7e67fdbfda5d623ba1ff
waddella <adrian.waddell@roche.com>
2017-09-22 16:32:51 +0200

    add a shell for time_to_event_table

---
+++ R/tm_time_to_event_table.R
@@ -0,0 +1,18 @@
+
+
+#' Time to Event Table
+#' 
+#' This is the description of the function
+#' 
+#' @param ATE time to event data.frame
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' time_to_event_table(NULL)
+time_to_event_table <- function(ATE) {
+  
+  table(iris$Species)
+  
+}
