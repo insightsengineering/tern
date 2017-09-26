4f447903bfae358fc2fec3c9a7c780a67eac47a5
waddella <adrian.waddell@roche.com>
2017-09-26 21:59:44 +0200

    design suggestions

--- R/tm_time_to_event_table.R
+++ R/tm_time_to_event_table.R
@@ -4,14 +4,31 @@
 #' 
 #' This is the description of the function
 #' 
-#' @param ATE time to event data.frame
+#' @param time_to_event time to event data
+#' @param is_event boolean with \code{TRUE} if event and \code{FALSE} if censored
+#' 
+#' 
+#' @return a named list with one element per row where each element contains the
+#'   data for the row
 #' 
 #' @export
 #' 
+#' 
 #' @examples 
+#' \dontrun{
+#' library(atezo.data)
+#' library(dplyr)
+#' ATE <- ate(com.roche.cdpt7722.wo29637.rl)
+#' ATE_f <- ATE %>% filter(PARAM == "OS")
+#' X <- time_to_event_table(
+#'     time_to_event = ATE_f$AVAL,
+#'     is_event = (ATE$CNSR == "N")
+#' )
+#'  
+#' }
 #' 
 #' time_to_event_table(NULL)
-time_to_event_table <- function(ATE) {
+time_to_event_table <- function(time_to_event, is_event) {
   
   table(iris$Species)
   
