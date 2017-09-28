eb1173616dc439f314aa8b437703bfe58698322a
waddella <adrian.waddell@roche.com>
2017-09-28 15:37:43 +0200

    refactor code

--- R/tm_forestplot.R
+++ R/tm_forestplot.R
@@ -1,36 +1,58 @@
-library(atezo.data)
-library(survival)
-source("~/teal.oncology/R/functions.R")
+split.sub <- function(data, variable, cutoff=NULL){
+  #create a temporary binary variable for numerical cutoff 
+  data[["temp"]] <- data[[variable]] 
+  if(is.numeric(data[[variable]])){
+    #if no user-defined cutoff, then median is the default
+    if(typeof(cutoff)=="NULL") {
+      data[["temp"]] <- ifelse(data[[variable]]<=median(data[[variable]],na.rm=T),"<=Median",">Median")
+    }else{
+      data[["temp"]] <- ifelse(data[[variable]]<=cutoff,paste("<=",cutoff,sep=""),paste(">",cutoff,sep=""))
+    }
+    data <- data[!is.na(data[[variable]]),]
+  }
+  if(!is.numeric(data[[variable]])){
+    data <- data[data[[variable]]!="",]
+  }
+  data <- data[,c("USUBJID","temp")]
+  return(split(data,data[["temp"]]))
+}
 
-ATE <- ate(com.roche.cdt30019.go29436.re)
-ASL <- asl(com.roche.cdt30019.go29436.re)
 
-
-#Subgroup ATE by the selected ASL variable categories;
-split.anl <- function (x,data){
+#' Subgroup ATE by the selected ASL variable categories;
+#' 
+#' 
+split.anl <- function (x, data){
   y <- merge(x,data,by = "USUBJID",all.x = T)
   return (y)
 }
 
-#Set up data list ;
-data.list <- function (adsl = ASL, groupvar, anl){
-  dlist           <- NULL
-  dlist$ALL       <- list(ALL = ATE)
-  for (i in 1:length(groupvar)){
-    dlist[[groupvar[i]]] <- lapply(split.sub(adsl, groupvar[i]), split.anl, anl = data)
-  }
-  return(dlist)
+#' Set up data list
+data.list <- function (adsl, groupvar, anl){
+  
+  dlist <- lapply(setNames(groupvar, groupvar), function(var) {
+    lapply(split.sub(adsl, var), split.anl, data = anl)
+  })
+  
+  c(list(ALL = list(ALL = anl)), dlist)
 }
 
 
-
+#' Create a survival table
+#' 
+#' @importFrom survival survfit Surv
 tab.surv <- function(data, outcome, arm.ref, arm.comp){
-  datatemp <- data[data[["ARM"]]%in%c(arm.ref,arm.comp),]
+  
+  # data %needs% c("ARM")
+  
+  datatemp <- data[data[["ARM"]] %in% c(arm.ref, arm.comp), ]
+  
   arm.ref.temp  <- paste(arm.ref,collapse = "/")
   arm.comp.temp <- paste(arm.comp,collapse = "/")
+  
   #create a temporary Arm variable  to store ref and comparable arms, useful when combined arm is required;
   datatemp[["armtemp"]] <- ifelse(datatemp[["ARM"]] %in% c(arm.ref.temp ), arm.ref.temp , arm.comp.temp)
   datatemp[["armtemp"]] <- factor(datatemp[["armtemp"]], levels = c(arm.ref.temp , arm.comp.temp))
+  
   #Create KM table;
   km     <- survfit(Surv(AVAL,CNSR == 1) ~ armtemp,data = datatemp[datatemp$PARAM == outcome,])
   km.tab <- round(summary(km)$table, 1)
@@ -41,26 +63,70 @@ tab.surv <- function(data, outcome, arm.ref, arm.comp){
   colnames(tab) <- c(paste(arm.ref.temp, "(Event/N)", sep = ""), 
                      paste(arm.comp.temp, "(Event/N)",sep=""), 
                      paste("MST", "(", arm.ref.temp, "/", arm.comp.temp, ")",sep=""))
-  return (tab)
+  
+  tab
+  
   #Add Cox-regression Table;
 }
 
-
-tab.list <- function (adsl = ASL, data = ATE, groupvar, outcome, arm.ref, arm.comp){
-   dlist <- data.list(adsl = ASL, data = ATE, groupvar = groupvar)
-   groupvar <- c("ALL", groupvar)
-   tlist <- NULL
-   Variable <- NULL
-  for (i in 1:length(groupvar)){
-    tlist[[i]] <- lapply(dlist[[groupvar[i]]], tab.surv, outcome = outcome, arm.ref = arm.ref, arm.comp = arm.comp)
-    tlist[[i]] <- do.call(rbind,tlist[[i]])
-    Subgroup   <- row.names(tlist[[i]])
-    tlist[[i]] <- cbind(Subgroup, tlist[[i]])
-    Variable   <- c(Variable, c(names(dlist[i]),rep("",length(Subgroup)-1)))
-  }
-  tab <- cbind(Variable, do.call(rbind,tlist))
-  row.names(tab) <- NULL
-  return (tab)
+#' create the time to event table
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' 
+#' \dontrun{
+#' library(atezo.data)
+#' 
+#' ATE <- ate(com.roche.cdt30019.go29436.re)
+#' ASL <- asl(com.roche.cdt30019.go29436.re)
+#' 
+#' tab.list(
+#'   adsl = ASL,
+#'   data = ATE,
+#'   groupvar = c("BAGE","BECOG", "SEX"),
+#'   outcome = "Overall Survival",
+#'   arm.ref = "DUMMY A",
+#'   arm.comp = c("DUMMY B", "DUMMY C")
+#' )
+#' }
+#' 
+#' 
+#' 
+tab.list <- function (adsl, data, groupvar, outcome, arm.ref, arm.comp){
+  
+  dlist <- data.list(adsl = adsl, anl = data, groupvar = groupvar)
+  
+  # x <- dlist[[1]]
+  surv_out <- lapply(dlist, function(x) {
+    # data_subgroup <- x[[1]]
+    lapply(x, function(data_subgroup) {
+      # kaplan meier summary
+      df_km <- tab.surv(data = data_subgroup, outcome = outcome,
+                           arm.ref = arm.ref, arm.comp = arm.comp)
+      
+      # cox model
+      arm2 <- factor(ifelse(data_subgroup$ARM == arm.ref, arm.ref, "comparison"))
+      
+      data_subgroup$armtemp <- relevel(arm2, ref = arm.ref)
+      coxm    <- summary( coxph( Surv(AVAL, CNSR == 0 ) ~ armtemp, data = data_subgroup))
+      
+      HR.95 <- coxm$conf.int[1]
+      HR.95lb <- coxm$conf.int[3]
+      HR.95ub <- coxm$conf.int[4]
+      p.val   <- coxm$coef[5]
+      
+      cbind(df_km, HR.95, HR.95lb, HR.95ub, p.val)
+    })
+  })
+  
+  surv_out2 <- unlist(surv_out, recursive = FALSE)
+  
+  X <- Reduce(rbind, surv_out2)
+  row.names(X) <-names(surv_out2)
+  
+  X
 }
 
-tab.list( adsl = ASL, data = ATE, groupvar = c("BAGE","BECOG", "SEX"), outcome = "Overall Survival", arm.ref = "DUMMY A", arm.comp = c("DUMMY B", "DUMMY C"))
+
