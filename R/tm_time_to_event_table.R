359535d1f584c1e5f4e6314533a1c5ed985a47be
waddella <adrian.waddell@roche.com>
2017-10-09 14:04:29 -0700

    design proposal

--- R/tm_time_to_event_table.R
+++ R/tm_time_to_event_table.R
@@ -18,25 +18,71 @@
 #' \dontrun{
 #' library(atezo.data)
 #' library(dplyr)
-#' ATE <- ate(com.roche.cdpt7722.wo29637.rl)
+#' 
+#' ATE <- ate(com.roche.cdt30019.go29436.re)
+#' 
+#' 
 #' ATE_f <- ATE %>% filter(PARAM == "OS")
-#' X <- time_to_event_table(
+#' 
+#' tbl <- time_to_event_table(
 #'     time_to_event = ATE_f$AVAL,
-#'     is_event = (ATE$CNSR == "N")
+#'     is_event = (ATE_f$CNSR == "N"),
+#'     arm = ATE_f$ARM,
+#'     arm.ref = "DUMMY A",
+#'     arm.rest = c("DUMMY C", "DUMMY B")
 #' )
-#'  
-#' }
+#' 
+#' tbl_stream <- get_time_to_event_table(com.roche.cdt30019.go29436.re)
+#' 
+#' compare_table(tbl, tbl_stream)
 #' 
 #' time_to_event_table(NULL)
-time_to_event_table <- function(time_to_event, is_event) {
+#' 
+#' }
+time_to_event_table <- function(time_to_event, event, arm, arm.ref,
+                                arm.rest = setdiff(arm, arm.ref)) {
   
   ## first do math
   
+  ## check your arguments
+  
+  ARM <- recode(arm, arm.ref, arm.rest)
+  ## do arm codings (reference, etc)
+
+  surv_km_results <- survival::survfit(formula = Surv(time_to_event, event) ~ ARM, 
+                                       conf.type="plain")
+  
+  
+  surv_km_results_summary <- summary(surv_km_results)
+  surv_km_results_tidy <- broom::tidy(surv_km_results)
+  
   ## then to data struture
-  list(
-    "Patients with Events (%)" = list(
-      c(5, .2),
-      c(4, .4)
-    )
+  rtable(
+    colnames = c(""),
+    rrow()
   )
+}
+
+#' @export
+time_to_event_table_ADAM <- function(ASL, ATE, PARAMCD, ...) {
+  
+   ATE_f <- ATE %>% filter(PARAMCD == PARAMCD)
+   
+   tbl <- time_to_event_table(
+       time_to_event = ATE_f$AVAL,
+       is_event = (ATE_f$CNSR == "N"),
+       arm = ATE_f$ARM,
+       ...
+   )
+   
+   tbl
+  
+}
+
+
+drop_label <- function(x){
+  for (j in (1:ncol(x))){
+    attr(x[[j]],"label") <- NULL
+  }
+  return(x)
 }
