280b8fdb41b897ad12df1ed1a0a869cdbcbf3ba6
rothem1 <mark.rothe@roche.com>
2017-10-10 23:00:22 -0400

    A vs. C and B vs. C Top 3rd Commit.

--- R/tm_time_to_event_table.R
+++ R/tm_time_to_event_table.R
@@ -1,5 +1,3 @@
-
-
 #' Time to Event Table
 #' 
 #' This is the description of the function
@@ -17,113 +15,142 @@
 #' 
 #' @examples 
 #' \dontrun{
+#' library(teal.oncology)
 #' library(atezo.data)
 #' library(dplyr)
+#' library(survival)
+#' library(broom)
+#' library(stringr)
 #' 
-#' ATE <- ate(com.roche.cdt30019.go29436.re)
+#' ASL <- atezo.data::asl(com.roche.cdt30019.go29436.re)
+#' ATE <- atezo.data::ate(com.roche.cdt30019.go29436.re)
 #' 
-#' tbl_stream <- get_time_to_event_table(com.roche.cdt30019.go29436.re)
-#' Viewer(tbl_stream)
-#' 
-#' ATE_f <- ATE %>% filter(PARAMCD == "OS")
-#' 
-#' tbl <- time_to_event_table(
-#'     time_to_event = ATE_f$AVAL,
-#'     event = (ATE_f$CNSR == 0)*1,
-#'     arm = ATE_f$ARM1,
-#'     arm.ref = "DUMMY C"
-#' )
-#' 
-#' tbl
-#' 
-#' compare_rtables(tbl, tbl_stream, tol = 0.2)
-#' 
+#' tte_tbl_stream <- atezo.data::get_time_to_event_table(com.roche.cdt30019.go29436.re)
+#' teal.oncology::Viewer(tte_tbl_stream)
+#'
+#' ASL_f <- dplyr::filter(ASL,ITTFL == "Y")
+#'         
+#' ATE_f <- dplyr::filter(ATE,ITTFL == "Y" & PARAMCD == "OS")
 #' 
 #' }
-time_to_event_table <- function(time_to_event, event,
-                                arm, arm.ref,
-                                vars_strat
-                                ) {
-  
-  # argument checking
-  n <- length(time_to_event)
-  if (length(event) != n) stop("event has wrong length")
-  if (length(arm) != n) stop("arm has wrong length")
-  
-  if (length(arm.ref)!=1) stop("referemce arm to be expected length 1")
 
-  if (!(arm.ref %in% arm)) stop("arm.ref not in arm")
+time_to_event <- ATE_f$AVAL
+event <- ifelse(is.na(ATE_f$CNSR),NA,
+         ifelse(ATE_f$CNSR==1,0,1))
+arm = ATE_f$ARM1
+big_n_arm <- ASL_f$ARM1
+arm.ref = "DUMMY C"
+
   
   ARM <- factor(arm, levels = c(arm.ref, setdiff(arm, arm.ref)))
-
-  surv_km_fit <- survfit(
+  
+  levels(big_n_arm) <- as.vector(levels(ARM))
+  
+  surv_km_fit <- survival::survfit(
     formula = Surv(time_to_event, event) ~ ARM, 
     conf.type="plain"
   )
   
-  surv_km_results_summary <- summary(surv_km_fit)
-  surv_km_results_tidy <- broom::tidy(surv_km_fit)
+  surv_km_summary <- summary(surv_km_fit)
+  surv_km_table <- surv_km_summary$table
+  surv_km_broom <- broom::tidy(surv_km_fit)
+  ref_surv_km_ranges <- surv_km_broom[which(stringr::str_sub(surv_km_broom$strata,5,
+                                      nchar(surv_km_broom$strata)) == as.vector(levels(ARM))[1]),]$time
+
+  comp1_surv_km_ranges <- surv_km_broom[which(stringr::str_sub(surv_km_broom$strata,5,
+                                        nchar(surv_km_broom$strata)) == as.vector(levels(ARM))[2]),]$time
+
+  comp2_surv_km_ranges <- surv_km_broom[which(stringr::str_sub(surv_km_broom$strata,5,
+                                        nchar(surv_km_broom$strata)) == as.vector(levels(ARM))[3]),]$time
   
-  surv_km_results <- surv_km_results_summary$table
+  surv_km_quantile <- quantile(surv_km_fit)$quantile
+
+  # BIG N #
+  ref_BIG_N <- length(big_n_arm[big_n_arm == as.vector(levels(big_n_arm))[1]])
+  comp1_BIG_N <- length(big_n_arm[big_n_arm == as.vector(levels(big_n_arm))[2]])
+  comp2_BIG_N <- length(big_n_arm[big_n_arm == as.vector(levels(big_n_arm))[3]])
+
+  ref_big_n <- surv_km_table[1,1]
+  comp1_big_n <- surv_km_table[2,1]
+  comp2_big_n <- surv_km_table[3,1]
+  
+  if (as.numeric(ref_BIG_N) != ref_big_n) stop("Reference Population Differences Between ASL and ATE!")
+  if (as.numeric(comp1_BIG_N) != comp1_big_n) stop("Comparator 1 Population Differences Between ASL and ATE!")
+  if (as.numeric(comp2_BIG_N) != comp2_big_n) stop("Comparator 2 Population Differences Between ASL and ATE!")
+  
+  tte_big_n <- data.frame(ref_big_n,comp1_big_n,comp2_big_n)
+  
+  # Patients With Events #
+  ref_n_events <- surv_km_table[1,4]
+  comp1_n_events <- surv_km_table[2,4]
+  comp2_n_events <- surv_km_table[3,4]
+  
+  ref_p_events <- ref_n_events / ref_big_n
+  comp1_p_events <- comp1_n_events / comp1_big_n
+  comp2_p_events <- comp2_n_events / comp2_big_n
+
+  tte_np_events <- data.frame(ref_n_events,ref_p_events,comp1_n_events,comp1_p_events,
+                              comp2_n_events,comp2_p_events)
+  
+  # Patients Without Events #  
+  ref_n_wo_events <- ref_big_n - ref_n_events
+  comp1_n_wo_events <- comp1_big_n - comp1_n_events
+  comp2_n_wo_events <- comp2_big_n - comp2_n_events
+  
+  ref_p_wo_events <- ref_n_wo_events / ref_big_n
+  comp1_p_wo_events <- comp1_n_wo_events / comp1_big_n
+  comp2_p_wo_events <- comp2_n_wo_events / comp2_big_n
+
+  tte_np_wo_events <- data.frame(ref_n_wo_events,ref_p_wo_events,comp1_n_wo_events,comp1_p_wo_events,
+                                 comp2_n_wo_events,comp2_p_wo_events)
+  
+  # Median Time to Event #
+  ref_med_tte <- surv_km_table[1,7]
+  comp1_med_tte <- surv_km_table[2,7]
+  comp2_med_tte <- surv_km_table[3,7]
+  
+  ref_med_tte_lcl <- surv_km_table[1,8]
+  ref_med_tte_ucl <- surv_km_table[1,9]
+  
+  comp1_med_tte_lcl <- surv_km_table[2,8]
+  comp1_med_tte_ucl <- surv_km_table[2,9]
+
+  comp2_med_tte_lcl <- surv_km_table[3,8]
+  comp2_med_tte_ucl <- surv_km_table[3,9]
+ 
+  tte_median <- data.frame(ref_med_tte,ref_med_tte_lcl,ref_med_tte_ucl,
+                           comp1_med_tte,comp1_med_tte_lcl,comp1_med_tte_ucl,
+                           comp2_med_tte,comp2_med_tte_lcl,comp2_med_tte_ucl)
+
+  # 25% and 75% Time to Event Percentiles #
+  ref_25th <- surv_km_quantile[1,1]
+  ref_75th <- surv_km_quantile[1,3]
+  
+  comp1_25th <- surv_km_quantile[2,1]
+  comp1_75th <- surv_km_quantile[2,3]
+  
+  comp2_25th <- surv_km_quantile[3,1]
+  comp2_75th <- surv_km_quantile[3,3]
+
+  tte_quantiles <- data.frame(ref_25th,ref_75th,comp1_25th,comp1_75th,comp2_25th,comp2_75th)
+  
+  # Minimum and Maximum Time to Events #
+  ref_km_min <- min(ref_surv_km_ranges)
+  ref_km_max <- max(ref_surv_km_ranges)
+  
+  comp1_km_min <- min(comp1_surv_km_ranges)
+  comp1_km_max <- max(comp1_surv_km_ranges)
+  
+  comp2_km_min <- min(comp2_surv_km_ranges)
+  comp2_km_max <- max(comp2_surv_km_ranges
+                      )  
+  tte_range <- data.frame(ref_km_min,ref_km_max,comp1_km_min,comp1_km_max,comp2_km_min,comp2_km_max)
   
   
-  #----------------------#
-  # Top 3rd of TTE Table #
-  #----------------------#
-  ref_BIG_N <- surv_km_results[1,1]
-  comp_BIG_N <- surv_km_results[2,1]
+  tte_table <- list(tte_big_n,tte_np_events,tte_np_wo_events,tte_median,tte_quantiles,tte_range)
   
-  ref_n_events <- surv_km_results[1,4]
-  comp_n_events <- surv_km_results[2,4]
+  #return(tte_table)
   
-  ref_n_wo_events <- ref_BIG_N - ref_n_events
-  comp_n_wo_events <- comp_BIG_N - comp_n_events
-  
-  ref_p_events <- ref_n_events / ref_BIG_N
-  comp_p_events <- comp_n_events / comp_BIG_N
-  
-  ref_p_wo_events <- ref_n_wo_events / ref_BIG_N
-  comp_p_wo_events <- comp_n_wo_events / comp_BIG_N
-  
-  ref_med_tte <- surv_km_results[1,7]
-  comp_med_tte <- surv_km_results[2,7]
-  
-  ref_med_tte_lcl <- surv_km_results[1,8]
-  ref_med_tte_ucl <- surv_km_results[1,9]
-  
-  comp_med_tte_lcl <- surv_km_results[2,8]
-  comp_med_tte_ucl <- surv_km_results[2,9]
-  
-  
-  surv_km_quantiles <- quantile(surv_km_fit)$quantile
-  
-  ref_25th <- surv_km_quantiles[1,1]
-  ref_75th <- surv_km_quantiles[1,3]
-  
-  comp_25th <- surv_km_quantiles[2,1]
-  comp_75th <- surv_km_quantiles[2,3]
-  
-  surv_km_ranges <- broom::tidy(surv_km_fit)
-  
-  ref_km_min <- min(surv_km_ranges[which(stringr::str_sub(surv_km_ranges$strata,5,
-                                                          nchar(surv_km_ranges$strata)) == arm.ref),]$time,na.rm=TRUE)
-  
-  ref_km_max <- max(surv_km_ranges[which(stringr::str_sub(surv_km_ranges$strata,5,
-                                                          nchar(surv_km_ranges$strata)) == arm.ref),]$time,na.rm=TRUE)
-  
-#  comp_km_min <- min(surv_km_ranges[which(stringr::str_sub(surv_km_ranges$strata,5,
-#                                                           nchar(surv_km_ranges$strata)) == comp_arm),]$time,na.rm=TRUE)
-  
-#  comp_km_max <- max(surv_km_ranges[which(stringr::str_sub(surv_km_ranges$strata,5,
-#                                                           nchar(surv_km_ranges$strata)) == comp_arm),]$time,na.rm=TRUE)
-  
-#  tte_BIG_N <- data.frame(ref_BIG_N,comp_BIG_N)
-#  tte_np_events <- data.frame(ref_n_events,ref_p_events,comp_n_events,comp_p_events)
-#  tte_np_wo_events <- data.frame(ref_n_wo_events,ref_p_wo_events,comp_n_wo_events,comp_p_wo_events)
-#  tte_median <- data.frame(ref_med_tte,ref_med_tte_lcl,ref_med_tte_ucl,
-#                           comp_med_tte,comp_med_tte_lcl,comp_med_tte_ucl)
-#  tte_quantiles <- data.frame(ref_25th,ref_75th,comp_25th,comp_75th)
-#  tte_range <- data.frame(ref_km_min,ref_km_max,comp_km_min,comp_km_max)
   
   #-------------------------#
   # Middle 3rd of TTE Table #
@@ -153,14 +180,14 @@ time_to_event_table <- function(time_to_event, event,
 #  return(tte_table)
   
   ## then to data struture
-  tbl <- rtable(
-    col.names = c("DUMMY C\n(N=255)", "DUMMY B\n(N=254)", "DUMMY A\n(N=254)"),
-    format = "xx",
-    rrow("Responders", c(1, 0.1), c(2, .3), c(33, .43245), format = "xx (xx.xx%)"),
-    rrow("Non-Responders", c(3, 0.2), c(43,0.32), c(33, .3442), format = "xx (xx.xx%)"),
-    rrow(),
-    rrow("95% CI for Response Rates (Clopper−Pearson)", c(0,1), c(0, 2), c(4,5), format = "(xx.xx, xx.xx)")
-  )
+#tbl <- rtable(
+#    col.names = c("DUMMY C\n(N=255)", "DUMMY B\n(N=254)", "DUMMY A\n(N=254)"),
+#    format = "xx",
+#    rrow("Responders", c(1, 0.1), c(2, .3), c(33, .43245), format = "xx (xx.xx%)"),
+#    rrow("Non-Responders", c(3, 0.2), c(43,0.32), c(33, .3442), format = "xx (xx.xx%)"),
+#    rrow(),
+#    rrow("95% CI for Response Rates (Clopper−Pearson)", c(0,1), c(0, 2), c(4,5), format = "(xx.xx, xx.xx)")
+#  )
   
   # compare_rtables(tbl, tbl_stream, tol = 0.2)
   
@@ -183,10 +210,32 @@ time_to_event_table_ADAM <- function(ASL, ATE, PARAMCD, ...) {
   
 }
 
+#' tbl <- time_to_event_table(
+#'     time_to_event = ATE_f$AVAL,
+#'     event = (ATE_f$CNSR == 0)*1,
+#'     arm = ATE_f$ARM1,
+#'     arm.ref = "DUMMY C"
+#' )
+#' 
+#' tbl
+#' 
+#' compare_rtables(tbl, tbl_stream, tol = 0.2)
 
-drop_label <- function(x){
-  for (j in (1:ncol(x))){
-    attr(x[[j]],"label") <- NULL
-  }
-  return(x)
-}
+
+#'          dplyr::mutate(time_to_event   = AVAL,
+#'                        status = 
+#'                                 
+#'                                 #time_to_event_table <- function(time_to_event, event,
+#                                arm, arm.ref,
+#                                vars_strat
+#                                ) {
+ref_BIG_N <- table(big_n_arm)[[1]]
+comp1_BIG_N <- table(big_n_arm)[[2]]
+comp2_BIG_N <- table(big_n_arm)[[3]]
+# argument checking
+#  n <- length(time_to_event)
+#  if (length(event) != n) stop("event has wrong length")
+#  if (length(arm) != n) stop("arm has wrong length")
+#   if (!(arm.ref %in% arm)) stop("arm.ref not in arm")
+
+#  if (length(arm.ref)!=1) stop("referemce arm to be expected length 1")
