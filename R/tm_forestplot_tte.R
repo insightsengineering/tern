7e303554e95e1d50943cc36e60d0ab1595d769d3
songy24 <yuyao.song@roche.com>
2017-10-25 09:46:25 -0400

    updated labels. But plot is not working

--- R/tm_forestplot_tte.R
+++ R/tm_forestplot_tte.R
@@ -23,7 +23,7 @@
 #'        label = "Forest Survival",
 #'        endpoint = "OS",
 #'        endpoint_choices = unique(ATE$PARAMCD),
-#'        baseline_risk = c("BAGED", "SEX", "BECOG"),
+#'        subgroup_var = c("BAGED", "SEX", "BECOG"),
 #'        arm.ref = arms[1],
 #'        arm.ref_choices = arms,
 #'        arm.comp = arms[-1],
@@ -40,8 +40,8 @@ tm_forest_survival <- function(label,
                                arm.ref_choices = arm.ref,
                                arm.comp,
                                arm.comp_choices = arm.comp,
-                               baseline_risk,
-                               baseline_risk_choices = baseline_risk,
+                               subgroup_var,
+                               subgroup_var_choices = subgroup_var,
                                endpoint = "OS",
                                endpoint_choices = "OS",
                                plot_height = c(600, 200, 2000),
@@ -70,8 +70,8 @@ ui_forest_survival <- function(id, label,
                                arm.ref_choices = arm.ref,
                                arm.comp,
                                arm.comp_choices = arm.comp,
-                               baseline_risk,
-                               baseline_risk_choices = baseline_risk,
+                               subgroup_var,
+                               subgroup_var_choices = subgroup_var,
                                endpoint = "OS",
                                endpoint_choices = "OS",
                                plot_height,
@@ -90,7 +90,7 @@ ui_forest_survival <- function(id, label,
       optionalSelectInput(ns("endpoint"), "Time to Event", endpoint_choices, endpoint, multiple = FALSE),
       optionalSelectInput(ns("ref_arm"), "Reference Group", arm.ref_choices, arm.ref, multiple = TRUE),
       optionalSelectInput(ns("treat_arm"), "Treatment Group", arm.comp_choices, arm.comp, multiple = TRUE),
-      optionalSelectInput(ns("baseline_risk"), "Baseline Risk Factors", baseline_risk_choices, baseline_risk, multiple = TRUE)
+      optionalSelectInput(ns("subgroup_var"), "Subgroup Variables", subgroup_var_choices, subgroup_var, multiple = TRUE)
     ),
     #forms = actionButton(ns("show_rcode"), "Show R Code", width = "100%"),
     pre_output = pre_output,
@@ -109,7 +109,7 @@ srv_forest_survival <- function(input, output, session, datasets) {
     ASL_filtered <- datasets$get_data("ASL", reactive = TRUE, filtered = TRUE)
   
     endpoint <- input$endpoint
-    baseline_risk <- input$baseline_risk
+    subgroup_var <- input$subgroup_var
     ref_arm <- input$ref_arm
     treat_arm <- input$treat_arm
     
@@ -117,7 +117,7 @@ srv_forest_survival <- function(input, output, session, datasets) {
     teal:::as.global(ATE_filtered)
     teal:::as.global(ASL_filtered)
     teal:::as.global(endpoint)
-    teal:::as.global(baseline_risk)
+    teal:::as.global(subgroup_var)
     teal:::as.global(ref_arm)
     teal:::as.global(treat_arm)
     
@@ -140,37 +140,48 @@ srv_forest_survival <- function(input, output, session, datasets) {
     
     ## you need to add the encodings
     ATE_f <- ATE_filtered %>% 
-      filter(PARAMCD == endpoint, ARM %in% c(ref_arm, treat_arm))
+      filter(PARAMCD == endpoint) %>%
+      select(c("STUDYID", "USUBJID", "PARAMCD", "AVAL", "CNSR"))
     
     validate(need(nrow(ATE_f) > 0, "no data left"))
     
+    
     ASL_filtered$BAGED <- ifelse(ASL_filtered$BAGE <= median(ASL_filtered$BAGE), "<=median", ">median")
     
-    validate(need(all(baseline_risk %in% names(ASL_filtered)),
-                  "some baseline risk variables are not valid"))
     
-
+    ASL_f <- ASL_filtered %>% 
+      filter(ARM %in% c(ref_arm, treat_arm)) %>% 
+      select("STUDYID", "USUBJID", "ARM", subgroup_var) 
+    
+    validate(need(all(subgroup_var %in% names(ASL_f)),
+                  "some subgroup variables are not valid"))
+    ATE_f <- inner_join(ASL_f, ATE_f, by = c("STUDYID","USUBJID"))
       
     validate(need(all(c(ref_arm, treat_arm) %in% ATE_f$ARM), "data needs to include at least one patient from the reference and comparison arm"))  
     
-    group_by <- merge(
-      ATE_f[c("USUBJID", "STUDYID")],
-      ASL_filtered[c("USUBJID", "STUDYID", baseline_risk)],
-      all.x = TRUE, all.y = FALSE
-    )
+    group_by <- ATE_f %>% select(c("USUBJID", "STUDYID", subgroup_var))
+    head(group_by)
+
+    # group_by <- merge(
+    #   ATE_f[c("USUBJID", "STUDYID")],
+    #   ASL_filtered[c("USUBJID", "STUDYID", subgroup_var)],
+    #   all.y = FALSE, all.x = TRUE
+    # )
     
+    #head(group_by)
   
     ## add
-    ## the arm combine & filtering and converting to a factor here...
+    ## the arm combine & filtering and converting to a factor here...paste0(ref_arm, collapse = "/")
     ## using forcats
+    arm <- fct_collapse(ATE_f$ARM, ref_arm = ref_arm, treat_arm = treat_arm)
+    arm <- ifelse (arm == "ref_arm", paste0(ref_arm, collapse = "/"), paste0(treat_arm, collapse = "/")) 
+    arm <- fct_relevel(arm, paste0(ref_arm, collapse = "/"))
     
-    tbl <- surv_subgroup(
+    tbl <- forest_tte(
       time_to_event = ATE_f$AVAL,
       event = ATE_f$CNSR == 0,
-      arm = ATE_f$ARM, 
-      group_by = group_by[, -c(1,2), drop=FALSE],
-      arm.ref = ref_arm,
-      arm.comp = treat_arm
+      arm = arm, 
+      group_by = group_by[, -c(1,2), drop=FALSE]
     )
     
     as_html(tbl)
