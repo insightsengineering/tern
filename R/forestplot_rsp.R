8a4300c244f20709635f6635c6cb2ba0943e18f2
Song <songy24@nala.roche.com>
2018-02-09 11:01:49 -0500

    updated table function to convert large OR to >999.99

--- R/forestplot_rsp.R
+++ R/forestplot_rsp.R
@@ -41,29 +41,35 @@
 #' library(grid)
 #' library(teal.oncology)
 #' library(forcats)
+#' devtools::install_github("Roche/rtables")
 #' '%needs%' <- teal.oncology:::'%needs%'
 #' ARS <- ars(com.roche.cdt30019.go29436.re)
 #' ASL <- asl(com.roche.cdt30019.go29436.re)
 #' ASL$temp <- c(rep("A", 200), rep("B", 250), rep(NA,1202-450))
 #' 
-#' ARS_f <- ARS %>% filter(PARAMCD == "OVRSPI") %>% 
-#'                  filter(ITTWTFL == "N") %>% 
-#'                  filter(ARM %in% c("DUMMY A", "DUMMY C")) %>%
-#'                  select(c("USUBJID", "STUDYID", "SEX", "ICLEVEL", "TC3IC3", "ARM", "AVAL", "AVALC"))
-#' ASL_f <- ASL %>% filter(ITTWTFL == "N") %>% 
-#'              filter(ARM %in% c("DUMMY A", "DUMMY C")) %>% 
-#'              select(c("USUBJID", "STUDYID", "SEX", "MLIVER", "TCICLVL2", "AGE4CAT", "RACE", "temp"))
+#' ASL_f <- subset(ASL, 
+#'                 subset = (ITTWTFL == "N" & ARM %in% c("DUMMY A", "DUMMY C")), 
+#'                 select = c("USUBJID", "STUDYID", "ARM", "ARMCD", "SEX", "MLIVER", "TCICLVL2", "AGE4CAT", "RACE", "temp") )
+#' ARS_f <- subset(ARS, 
+#'                 subset = (PARAMCD == "OVRSPI"),
+#'                 select = c("STUDYID", "USUBJID", "PARAMCD", "PARAM", "AVAL", "AVALC"))
+#' ANL <- merge(ASL_f, ARS_f, by = c("STUDYID", "USUBJID"), all.x = TRUE, all.y = FALSE)
 #' 
-#' group_data <- left_join(ASL_f, ATE_f %>% select(c("USUBJID", "STUDYID")))
+#' group_data <- subset(ASL_f, select = c("USUBJID", "STUDYID", "SEX", "MLIVER", "TCICLVL2", "AGE4CAT", "RACE"))
+#' 
+#' for(x in names(group_data)){
+#'   attr(group_data[[x]], "label") <- attr(ASL[[x]], "label")
+#' }   
+#'
 #' names(group_data) <- labels_over_names(group_data)
 #' 
 #' head(group_data)
 #' 
-#' arm <- fct_relevel(ARS_f$ARM, "DUMMY C")
+#' arm <- fct_relevel(ASL_f$ARM, "DUMMY C")
 #' 
 #' tbl <- forest_rsp(
-#'           response = ARS_f$AVAL,
-#'           event = ARS_f$AVALC %in% c("CR","PR"),
+#'           response = ANL$AVAL,
+#'           event = ANL$AVALC %in% c("CR","PR"),
 #'           arm = arm, 
 #'           group_data = group_data[, -c(1,2), drop=FALSE]
 #' )
@@ -80,34 +86,20 @@ forest_rsp <- function(response, event,
   n <- length(response)
   if (length(event) != n) stop("event has wrong length")
   if (length(arm) != n) stop("arm has wrong length")
+  if (length(levels(arm)) <2) stop("need at least two levels in arm for logistic regression") 
   if (!is.data.frame(group_data)) stop("group_data is expected to be a data.frame")
   if (nrow(group_data) != n) stop("group_data has wrong number of rows")
   
   
   glm_data <- data.frame(response, event,arm)
 
-  # var = names(group_data)[1]
-  # split data into a tree for data
-  # where each leaf is a data.frame with 
-  # the data to compute the glm analysis with
-  # data_list <- c(
-  #   list(ALL = list(ALL = glm_data)),
-  #   lapply(group_data, function(var) {
-  #     sub_data <- lapply(setNames(unique(var[var != ""]), unique(var[var != ""])), function(value) {
-  #        glm_data[var == value , , drop= FALSE] 
-  #     })
-  #     sub_data[order(names(sub_data),decreasing = F)]
-  #   })
-  # )
-  
+  #Split data to subgroup lists. e.g. data$SEX$FEMALE
   data_list <- c(
     list(ALL = list(ALL = glm_data)),
     lapply(group_data, function(var) {
       sub_data <- cbind(glm_data, var)
       sub_data <- subset(sub_data, var != "")
-      #sub_data <- sub_data %>% filter(var != "")
       if ("" %in% levels(sub_data$var)) sub_data$var <- factor(sub_data$var, levels = levels(sub_data$var)[-which(levels(sub_data$var) == "")])
-      #   sub_data$var <- as.factor(as.character(sub_data$var))
       sub_data$var <- as.factor(sub_data$var)
       lapply(split(sub_data, sub_data$var), function(x){
         x[,-4]
@@ -115,8 +107,8 @@ forest_rsp <- function(response, event,
     })
   )
   
-  #varname=data_list$TC3IC3
-  #data_for_value = varname[1]
+  #varname=data_list$AGE4CAT
+  #data_for_value = varname[[4]]
   #apply the glm analysis
   results_glm <- lapply(data_list, function(varname) {
     lapply(varname, function(data_for_value) {
@@ -155,19 +147,23 @@ forest_rsp <- function(response, event,
         list(
           if (is_new_category) rrow() else NULL,
           if (is_new_category) rrow(last_header) else NULL,
-          rrow(
-            row.name = header_row_name[2],
-            x$resp_ref_n + x$resp_comp_n, # total n
-            x$resp_ref_n,
-            x$resp_ref_event,
-            rcell(x$resp_ref_event / x$resp_ref_n * 100, format = "xx.x"),
-            x$resp_comp_n,
-            x$resp_comp_event,
-            rcell(x$resp_comp_event / x$resp_comp_n * 100, format = "xx.x"),
-            rcell(x$glm_or, format = "xx.xx"),
-            rcell(c(x$glm_lcl, x$glm_ucl), format = "(xx.xx, xx.xx)"),
-            indent = if (header_row_name[1] == "ALL") 0 else 1
-          )
+             rrow(
+              row.name = header_row_name[2],
+              x$resp_ref_n + x$resp_comp_n, # total n
+              x$resp_ref_n,
+              x$resp_ref_event,
+              rcell(x$resp_ref_event / x$resp_ref_n * 100, format = "xx.x"),
+              x$resp_comp_n,
+              x$resp_comp_event,
+              rcell(x$resp_comp_event / x$resp_comp_n * 100, format = "xx.x"),
+              if (x$glm_or > 999.99 & !is.na(x$glm_or)) {
+                rcell(">999.99", format = "xx")
+                } else {
+                rcell(x$glm_or, format = "xx.xx")
+                },
+              rcell(c(x$glm_lcl, x$glm_ucl), format = "(xx.xx, xx.xx)"),
+              indent = if (header_row_name[1] == "ALL") 0 else 1
+            )
         )
       }, split(X, 1:nrow(X)), rownames(X)),
       recursive = FALSE)
@@ -219,7 +215,7 @@ forest_rsp <- function(response, event,
 #' library(atezo.data)
 #' library(dplyr) 
 #' library(grid)
-#' library(teal.oncology)
+#' library(teal.oncology) 
 #' library(forcats)
 #' 
 #' '%needs%' <- teal.oncology:::'%needs%'
@@ -371,13 +367,8 @@ draw_header_rsp <- function(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12) {
 glm_results <- function(data){
   
   #Response Rate
-  
-  tbl_arm <- table(data$arm)
-  resp_ref_n <- tbl_arm[names(tbl_arm)==levels(data$arm)[1]]
-  resp_comp_n <- tbl_arm[names(tbl_arm)==levels(data$arm)[2]]
-  if (is.na(resp_ref_n)) resp_ref_n = 0
-  if (is.na(resp_comp_n)) resp_comp_n = 0
-  
+  resp_n <- setNames(table(data$arm), c("resp_ref_n", "resp_comp_n"))
+
   tbl_freq <- table(data$event,data$arm)
   resp_ref_event <- tbl_freq[rownames(tbl_freq)=="TRUE",colnames(tbl_freq)==levels(data$arm)[1]]
   resp_comp_event <- tbl_freq[rownames(tbl_freq)=="TRUE",colnames(tbl_freq)==levels(data$arm)[2]]
@@ -385,19 +376,20 @@ glm_results <- function(data){
   if (length(resp_comp_event)==0) resp_comp_event = 0
   
   #Logistic Model
-  if (length(levels(as.factor(as.character(data$arm)))) == 2){
-  glm_model <- glm(event ~ arm, family=binomial(link='logit'), data = data)
-  glm_sum  <- summary(glm_model )
-  glm_or   <- exp(glm_sum$coefficient[2,1])
-  glm_lcl  <- exp(confint(glm_model)[2,1])
-  glm_ucl  <- exp(confint(glm_model)[2,2])
-  glm_pval <- glm_sum$coefficients[2,4]
+  if (length(levels(factor(data$arm))) == 2){
+     glm_model <- glm(event ~ arm, family=binomial(link='logit'), data = data)
+     glm_sum  <- summary(glm_model )
+     #glm_or   <- ifelse(exp(glm_sum$coefficient[2,1]) > 999, ">999.99", exp(glm_sum$coefficient[2,1]))
+     glm_or   <- exp(glm_sum$coefficient[2,1])
+     glm_lcl  <- suppressWarnings(exp(suppressMessages(confint(glm_model)[2,1])))
+     glm_ucl  <- suppressWarnings(exp(suppressMessages(confint(glm_model)[2,2])))
+     glm_pval <- glm_sum$coefficients[2,4]
   
-  resp_table <- data.frame(resp_ref_n, resp_comp_n, 
+  resp_table <- data.frame(resp_ref_n = resp_n[1], resp_comp_n = resp_n[2], 
                            resp_ref_event, resp_comp_event, 
                            glm_or, glm_lcl, glm_ucl, glm_pval)
   }else {
-  resp_table <- data.frame(resp_ref_n, resp_comp_n, 
+  resp_table <- data.frame(resp_ref_n = resp_n[1], resp_comp_n = resp_n[2],  
                              resp_ref_event, resp_comp_event, 
                              glm_or = NA, glm_lcl = NA, glm_ucl = NA, glm_pval = NA)
   }
