aa3749eee4565e0b2740c03ee76d80e3cf41753e
liaoc10 <chendi.liao@roche.com>
2018-03-22 00:06:26 -0400

    by-visit table can display empty or NE

--- R/t_summarize_by_visit.R
+++ R/t_summarize_by_visit.R
@@ -1,6 +1,26 @@
-#' Summarize Information by Visit
+#' Summary Table by Visit
 #' 
+#' This function summarizes test results or change from baseline statistics by 
+#' visit. Corresponds to STREAM table templates EGT01, VST01 and LBT01.
+#' 
+#' @inheritParams rtables::rtabulate.numeric
+#' @param data data frame with numerical variables to be summarized. If the 
+#'   variable has a \code{label} attribute then it will be used as the column 
+#'   name.
+#' @param visit factor with visit names ordered by desired display order in the
+#'   stacked table.
+#' @template param_col_by
+#'   
+#'   
 #' @export
+#' 
+#' @template return_rtable
+#' @template author_liaoc10
+#'   
+#' @details
+#' 
+#' 
+#' 
 #' 
 #' @examples 
 #' 
@@ -16,61 +36,48 @@
 #' ANL$ARM <- factor(ANL$ARM)
 #' ANL$VISIT <- factor(ANL$VISIT)
 #' 
+#' ANL <- var_relabel(ANL, AVAL = "Value at\nVisit", CHG = "Change from\nBaseline")
 #' 
 #' 
-#' t_summarize_by_visit(x = ANL$AVAL, visit = ANL$VISIT, col_by = ANL$ARM)
 #' 
-#' t_summarize_by_visit(x = ANL$CHG, visit = ANL$VISIT, col_by = ANL$ARM)
+#' t_summarize_by_visit(data = ANL$AVAL, visit = ANL$VISIT, col_by = ANL$ARM)
+#' 
+#' t_summarize_by_visit(data = ANL$CHG, visit = ANL$VISIT, col_by = ANL$ARM)
 #' 
 #' arm <- as.character(ANL$ARM)
 #' ARM <- factor(c(arm, paste0(arm,"-chng")), levels = c("ARM A", "ARM A-chng", "ARM B", "ARM B-chng"))
 #' 
-#' t_summarize_by_visit(x = c(ANL$AVAL, ANL$CHG), visit = rep(ANL$VISIT, 2), ARM)
+#' t_summarize_by_visit(data = c(ANL$AVAL, ANL$CHG), visit = rep(ANL$VISIT, 2), ARM)
 #' 
-t_summarize_by_visit <- function(x, visit, col_by) {
+t_summarize_by_visit <- function(data, visit, col_by) {
   
-  if (!is.factor(visit)) stop("...")
-  if (!is.factor(col_by)) stop("...")
+  # Check Arguments
+#  check_same_N(data = data, col_by = col_by, omit.NULL = FALSE)
+#  if (!is.data.frame(data)) stop("data is expected to be a data frame")  
+#  if (!is.factor(visit)) stop("visit is expected to be a factor")
+#  check_col_by(col_by, 1)
   
-  
-  is_na <- is.na(x)
-  x[is_na] <- -999
-  
-  df <- data.frame(x, col_by, is_na)
+  df <- data.frame(data, col_by)
   df_s <- split(df, visit)
   
 
-  iqr_num <- function(x) {
-    quantile(x, probs = c(.25, .75))
-  }
-
-
-  # dfi <- df_s[[1]]
-  # visit_name <- names(df_s)[1]
+  #dfi <- df_s[[1]]
+  #visit_name <- names(df_s)[1]
   tbls <- Map(function(dfi, visit_name) {
     tbl_i <- rbind(
       rtable(header = levels(col_by), rrow(visit_name)),
-      rtabulate(dfi$x, dfi$col_by, n_not_na, row.name = "n", indent = 1),
-      rtabulate(dfi$x, dfi$col_by, mean_sd, format = "xx.xx (xx.xx)", row.name = "Mean (SD)", indent = 1),
-      rtabulate(dfi$x, dfi$col_by, median_t, row.name = "Median", indent = 1, format = "xx.xx"),
-      rtabulate(dfi$x, dfi$col_by, iqr_num, row.name = "IQR", indent = 1, format = "xx.xx - xx.xx"),
-      rtabulate(dfi$x, dfi$col_by, range_t, format = "xx.xx - xx.xx", row.name = "Min - Max", indent = 1)
+      rtabulate(dfi$data, dfi$col_by, n_not_na3, row.name = "n", indent = 1),
+      rtabulate(dfi$data, dfi$col_by, mean_sd3, format = "xx.xx (xx.xx)", row.name = "Mean (SD)", indent = 1),
+      rtabulate(dfi$data, dfi$col_by, median_t3, row.name = "Median", indent = 1, format = "xx.xx"),
+      rtabulate(dfi$data, dfi$col_by, iqr_num3, row.name = "IQR", indent = 1, format = "xx.xx - xx.xx"),
+      rtabulate(dfi$data, dfi$col_by, range_t3, format = "xx.xx - xx.xx", row.name = "Min - Max", indent = 1)
     )
     dfi_s <- split(dfi, dfi$col_by)
-    
-    for (col_i in 1:length(dfi_s)) {
-      cell_data <- dfi_s[[col_i]]
-      if (all(cell_data$is_na)) {
-        for (ir in 2:6) {
-          tbl_i[[ir]][[col_i]] <- rcell(" ") 
-        }
-      }
-    }
     
     tbl_i
   }, df_s, names(df_s))
   
-  do.call(stack_rtables,tbls)
+  stack_rtables_l(tbls)
   
 }
 
--- R/tabulate_aggregation_functions.R
+++ R/tabulate_aggregation_functions.R
@@ -6,7 +6,6 @@ positives_and_proportion <- function(x) {
   sum(x) * c(1, 1/length(x))
 }
 
-
 n_not_na <- function(x) {
   sum(!is.na(x))
 }
@@ -21,4 +20,64 @@ median_t <- function(x, na.rm = TRUE) {
 
 range_t <- function(x, na.rm = TRUE) {
   range(x, na.rm = TRUE)
+}
+
+iqr_num <- function(x, na.rm = TRUE) {
+  quantile(x, probs = c(.25, .75), na.rm = TRUE)
+}
+
+
+# Version with test for if-all-NA, then display empty cell
+
+positives_and_proportion2 <- function(x) {
+  if (!is.logical(x)) stop("x is required to be logical")
+  if (all(is.na(x))) rcell(" ", format="xx") else sum(x) * c(1, 1/length(x))
+}
+
+n_not_na2 <- function(x) {
+  if (all(is.na(x))) rcell(" ", format="xx") else sum(!is.na(x))
+}
+
+mean_sd2 <- function(x, na.rm = TRUE) {
+  if (all(is.na(x))) rcell(" ", format="xx") else c(mean(x, na.rm = na.rm), sd(x, na.rm = na.rm))
+}
+
+median_t2 <- function(x, na.rm = TRUE) {
+  if (all(is.na(x))) rcell(" ", format="xx") else median(x, na.rm = TRUE)
+}
+
+range_t2 <- function(x, na.rm = TRUE) {
+  if (all(is.na(x))) rcell(" ", format="xx") else range(x, na.rm = TRUE)
+}
+
+iqr_num2 <- function(x, na.rm = TRUE) {
+  if (all(is.na(x))) rcell(" ", format="xx") else quantile(x, probs = c(.25, .75), na.rm = TRUE)
+}
+
+
+# Version with test for if-all-NA, then display NE
+
+positives_and_proportion3 <- function(x) {
+  if (!is.logical(x)) stop("x is required to be logical")
+  if (all(is.na(x))) rcell("NE", format="xx") else sum(x) * c(1, 1/length(x))
+}
+
+n_not_na3 <- function(x) {
+  sum(!is.na(x))
+}
+
+mean_sd3 <- function(x, na.rm = TRUE) {
+  if (all(is.na(x))) rcell("NE", format = function(x, output) paste0(x," (",x,")")) else c(mean(x, na.rm = na.rm), sd(x, na.rm = na.rm))
+}
+
+median_t3 <- function(x, na.rm = TRUE) {
+  if (all(is.na(x))) rcell("NE", format="xx") else median(x, na.rm = TRUE)
+}
+
+range_t3 <- function(x, na.rm = TRUE) {
+  if (all(is.na(x))) rcell("NE", format = function(x, output) paste0(x," - ",x)) else range(x, na.rm = TRUE)
+}
+
+iqr_num3 <- function(x, na.rm = TRUE) {
+  if (all(is.na(x))) rcell("NE", format = function(x, output) paste0(x," - ",x)) else quantile(x, probs = c(.25, .75), na.rm = TRUE)
 }
