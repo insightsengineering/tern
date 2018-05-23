bccac4ea5a7295343947a7e5fb5925eace975d55
waddella <adrian.waddell@roche.com>
2018-05-23 22:06:13 +0200

    work on waterfall plot

--- NEWS.md
+++ NEWS.md
@@ -1,10 +1,13 @@
 
 # tern 0.5.1
 
+## New TLGs
+* `t_ae_ctc`: 
+
 ## Package Internal Changes
 
 * added function `compound_table`
-* In forest plot functions, added formatting to display extreme values to ">999.9"." 
+* in forest plot functions, added formatting to display extreme values to ">999.9"." 
 * added `t_ae_ctc` adverse events table by highest NCI CTCAE grade function.
 
 # tern 0.5.0
--- R/g_wf.R
+++ R/g_wf.R
@@ -3,21 +3,28 @@
 #' This basic waterfall plot visualizes a quatity \code{height} ordered by value with some
 #' markup
 #'
-#' @param id vector of IDs used as the x-axis label for the waterall bars 
 #' @param height  numeric vector to be plotted as the waterfall bars
+#' @param id vector of IDs used as the x-axis label for the waterall bars 
 #' @param col vector of a categorical variable for bar coloring
 #' @param xlab x label. Default is \code{ID}.
 #' @param ylab y label. Default is \code{Value}.
-#' @param plot.title A string to be displayed as plot title.
+#' @param title A string to be displayed as plot title.
 #' @param col.legend.title A string to be displayed as legend title.
 #' 
 #' @template author_song24
 #' 
-#' @import ggplot2
+#' @importFrom ggplot2 ggplot aes theme geom_col geom_text labs xlab ylab
+#'   element_text element_blank element_rect scale_fill_manual
 #' 
 #' @export
 #' 
 #' @examples 
+#' 
+#' 
+#' g_wf(height = c(3,5,-1), id = letters[1:3])
+#' 
+#' g_wf(height = c(3,5,-1), id = letters[1:3], col = c("red", "green", "red"))
+#'
 #' 
 #' library(random.cdisc.data)
 #' library(dplyr)
@@ -51,15 +58,16 @@
 #'   id = paste("asdfdsfdsfsd",asld$USUBJID),
 #'   col = asld$SEX
 #' )
+#' 
 #' g_wf(
 #'   height = asld$pchg,
 #'   id = paste("asdfdsfdsfsd",asld$USUBJID),
 #'   xlab = "ID",
 #'   ylab = "Percentage Change",
-#'   plot.title = "Waterfall plot"
+#'   title = "Waterfall plot"
 #' )
 #' 
-g_wf <- function(height, id, col=NULL, xlab=NULL, ylab=NULL, col.legend.title=NULL, plot.title = NULL){
+g_wf <- function(height, id, col=NULL, xlab=NULL, ylab=NULL, col.legend.title=NULL, title = NULL){
   
   if (!is.null(col)) check_same_N(height = height, id=id, col = col) else check_same_N(height = height, id=id)
   
@@ -76,36 +84,40 @@ g_wf <- function(height, id, col=NULL, xlab=NULL, ylab=NULL, col.legend.title=NU
   plot_data <- data.frame(
     height = height,
     id = as.character(id),
-    col = if (is.null(col)) "gray90" else to_n(col, length(height)),
+    col = if (is.null(col)) "x" else to_n(col, length(height)),
     stringsAsFactors = FALSE
   )
   
   plot_data_ord <- plot_data[order(plot_data$height, decreasing = TRUE), ]
   
-  p <- ggplot(plot_data_ord, aes(x = factor(id, levels=id), y = height, fill = col)) +
+  p <- ggplot(plot_data_ord, aes(x = factor(id, levels=id), y = height)) +
     geom_col() + 
-    geom_text(label = format(plot_data_ord$height, digits=2),
+    # geom_hline(yintercept = 0) +
+    geom_text(label = format(plot_data_ord$height, digits = 2),
               vjust = ifelse(plot_data_ord$height >= 0, -0.5, 1.5)) +
     xlab(xlab) +
     ylab(ylab) + 
     theme(axis.text.x = element_text(angle = 90, hjust = 0, vjust = .5)) 
 
-    if (!is.null(col)) {
-      p <- p +
-        labs(fill = col.legend.title) +
-        theme(
-          legend.position="bottom",
-          legend.background = element_blank(),
-          legend.title = element_text(face="bold"),
-          legend.box.background = element_rect(colour = "black")
-        )
-    }
-    if (!is.null(plot.title)) {
-      p <- p +
-        labs( title = plot.title) +
-        theme(plot.title = element_text(face = "bold"))
-    }
+  if (!is.null(col)) {
+    p <- p +
+      aes(fill = col) +
+      # scale_fill_manual(values = col) +
+      labs(fill = col.legend.title) +
+      theme(
+        legend.position = "bottom",
+        legend.background = element_blank(),
+        legend.title = element_text(face="bold"),
+        legend.box.background = element_rect(colour = "black")
+      )
+  }
+  
+  if (!is.null(title)) {
+    p <- p +
+      labs( title = title) +
+      theme(plot.title = element_text(face = "bold"))
+  }
+  
   p
-
 
 }
