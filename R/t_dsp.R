9d866914168a0041f8078cd48f0a95d68cd61534
Li, Jennifer {MDBB~South San Francisco} <li.jing@gene.com>
2019-05-09 19:25:16 +0200

    add document

--- R/t_dsp.R
+++ R/t_dsp.R
@@ -1,3 +1,17 @@
+#' Single element for disposition table
+#'
+#' @inheritParams rtables::rtabulate.numeric
+#' @param x a factor or logical vector.
+#' @param col_by The group variable to define columns.
+#' @param col_N  The column total for each group that is displayed in the table header with (N=xx).
+#' @param row.name Only applicable when x is a logical vector. A string to label the row name. Default is "TRUE".
+#' @param indent An integer specifing the spaces before row.name.
+#' @param subset A logical vector with the same length as x that defines the subset
+#' @param ... arguments passed on to methods
+#'
+#' @export
+#'
+#' @examples
 #'
 #' library(purrr)
 #' dsp <- purrr::partial(t_dsp, col_by = ADSL0$ARM)
@@ -11,29 +25,31 @@
 #'   dsp(ADSL0$GOTTRT == "Y", row.name = "Received treatment"),
 #'   dsp(ADSL0$DISTRTFL == "Y", row.name = "Discontinued treatment"),
 #'   dsp(ADSL0$DRSCAT == "Safety", row.name = "Safety", indent = 1),
-#'   dsp(ADSL0$TRTDRS, subcategory = ADSL0$DRSCAT %in% "Safety", indent = 2),
+#'   dsp(ADSL0$TRTDRS, subset = ADSL0$DRSCAT %in% "Safety", indent = 2),
 #'   dsp(ADSL0$DRSCAT == "Other", row.name = "Other", indent = 1),
-#'   dsp(ADSL0$TRTDRS, subcategory = ADSL0$DRSCAT %in% "Other", indent = 2)
+#'   dsp(ADSL0$TRTDRS, subset = ADSL0$DRSCAT %in% "Other", indent = 2)
 #' )
 #'
 
-t_dsp <- function(x = x, col_by, col_N = table(col_by), subcategory = NULL,
-                  total = "All Patients", indent = 0, row.name = "TRUE",
-                  show_n = FALSE) {
+t_dsp <- function(x = x, col_by, col_N = table(col_by), subset = NULL,
+                  indent = 0, row.name = NULL) {
 
   stopifnot(is.factor(col_by), length(col_N) == nlevels(col_by))
   is.atomic(x) & (is.factor(x) | is.logical(x)) || stop("x is required to be atomic factor or logical vector")
 
-  if (is.null(subcategory)) {
-    subcategory <- rep(TRUE, length(x))
+  if (is.null(subset)) {
+    subset <- rep(TRUE, length(x))
   }
 
-  stopifnot(is.logical(subcategory), length(subcategory) == length(x))
-  subcategory[is.na(subcategory)] <- FALSE
+  stopifnot(is.logical(subset), length(subset) == length(x))
+  subset[is.na(subset)] <- FALSE
 
-  x_anl <- x[subcategory]
-  col_by <- col_by[subcategory]
+  x_anl <- x[subset]
+  col_by <- col_by[subset]
 
+  if(is.factor(x) & !is.na(row.name)) {
+    warning('x is factor and row.name will be ignored')
+  }
   label <- if (is.null(row.name)) {
     "TRUE"
   } else {
@@ -49,9 +65,9 @@ t_dsp <- function(x = x, col_by, col_N = table(col_by), subcategory = NULL,
     )
     row.names(tbl)[2] <- label
 
-    if(!show_n){
-      tbl <- tbl[2, ]
-    }
+    # n row is not shown in disposition table
+    tbl <- tbl[2, ]
+
 
   }else if(is.factor(x)){
     x_anl <- droplevels(x_anl)
