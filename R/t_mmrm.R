431f1556f665562aac13b88ad51338e2be1667ee
Li, Jennifer {MDBB~South San Francisco} <li.jing@gene.com>
2020-01-28 14:59:09 -0800

    mmrm (#231)
    * Add MMRM function

---
+++ R/t_mmrm.R
@@ -0,0 +1,407 @@
+#' Mix model with repeated measurements (MMRM) model
+#'
+#' The MMRM table function summarizes MMRM test results by visit and groups. The
+#' function produces adjusted lsmeans and standard error, as well as conducts
+#' comparisons between groups' adjusted means, where the first level of the group
+#' is the reference level.
+#'
+#' @details \url{https://pages.github.roche.com/NEST/docs/bookdown/NEST/statistics_clinical_trials/master/mixed-effect-model-repeat-measurement.html}
+#'
+#' Create rtable of MMRM test results
+#'
+#' @inheritParams a_mmrm
+#' @inheritParams argument_convention
+#'
+#' @return rtable object or table tree, depending on the the `table_tree` argument
+#'
+#' @export
+#'
+#' @seealso \code{\link{a_mmrm}}
+#'
+#' @examples
+#' library(dplyr)
+#' library(random.cdisc.data)
+#'
+#' ADSL <- radsl(cached = TRUE)
+#' ADQS <- radqs(cached = TRUE)
+#' ADQS_f <- ADQS %>%
+#'   filter(PARAMCD=="FKSI-FWB" & ABLFL != "Y" & ABLFL2 != "Y") %>%
+#'   droplevels()
+#'
+#' t_mmrm(formula = AVAL ~ ARM + AVISIT + STRATA1 + BMRKR2 + BASE + ARM*AVISIT,
+#'        data = ADQS_f,
+#'        id_var = "USUBJID",
+#'        col_N = table(ADSL$ARM),
+#'        mode = "boot-satterthwaite",
+#'        conf.level = 0.95,
+#'        table_tree = FALSE,
+#'        weights_emmeans = "proportional",
+#'        corStruct = "corSymm"
+#' )
+#'
+t_mmrm <- function(formula = AVAL ~ arm(ARM) + visit(AVISIT) + ARM * VISIT,
+                   data,
+                   id_var = "USUBJID",
+                   arm_var = "ARM",
+                   visit_var = "AVISIT",
+                   col_N = table(ADSL$ARM),
+                   mode = c("df.error", "auto", "boot-satterthwaite"),
+                   conf.level = 0.95,
+                   weights_emmeans = "proportional",
+                   corStruct = NULL,
+                   table_tree = TRUE) {
+
+  mmrm_result <- a_mmrm(formula = formula,
+                        data = data,
+                        id_var = id_var,
+                        arm_var = arm_var,
+                        visit_var = visit_var,
+                        mode = mode,
+                        conf.level = conf.level,
+                        weights_emmeans = weights_emmeans,
+                        corStruct = corStruct)
+
+  s_contrast_df <- split(
+    mmrm_result$`contrast`,
+    mmrm_result$`contrast`[visit_var]
+  )
+
+  s_estimate_df <- split(
+    mmrm_result$`estimate`,
+    mmrm_result$`estimate`[visit_var]
+  )
+
+  arm_lvl <- levels(data[[arm_var]])
+
+  tbl_head <- rheader(rrowl("", arm_lvl))
+
+  format_pval <- function(x, output) {
+    if (x < 0.0001) {
+      "<.0001"
+    } else {
+      paste(round(x, 4))
+    }
+  }
+
+  mmrm_node_list <- Map(function(df1_i, df2_i, visit){
+
+    tbl <- rtable(
+      header = tbl_head,
+      rrowl("n",
+            tapply(df1_i$`n`, factor(df1_i[[arm_var]], levels = arm_lvl),
+                   function(n_i) {
+                     c(n_i)
+                   }),
+            format = "xx"
+      ),
+      rrowl("Adjusted Mean (SE)",
+            lapply(split(df1_i,
+                         factor(df1_i[[arm_var]], levels = arm_lvl),
+                         drop = FALSE),
+                   function(vector_i) {
+                     if (is.null(vector_i)) {
+                       NULL}
+                     else {
+                       c(vector_i$`emmean`, vector_i$`SE`)
+                     }
+                   }),
+            format = sprintf_format("%.3f (%.3f)")
+      ),
+      rrowl(paste0(mmrm_result$`conf_level`*100, "% CI"),
+            lapply(split(df1_i,
+                         factor(df1_i[[arm_var]], levels = arm_lvl),
+                         drop = FALSE),
+                   function(vector_i) {
+                     if (is.null(vector_i)) {
+                       NULL}
+                     else {
+                       c(vector_i$`lower.CL`, vector_i$`upper.CL`)
+                     }
+                   }),
+            format = "(xx.xxx, xx.xxx)"
+      ),
+      rrow(),
+      rrowl(paste0("Difference in Adjusted Means (SE) (vs. ", mmrm_result$`ref_level`, ")"),
+            lapply(split(df2_i,
+                         factor(df2_i[[arm_var]], levels = arm_lvl),
+                         drop = FALSE),
+                   function(vector_i) {
+                     if (is.null(vector_i)) {
+                       NULL
+                     } else {
+                       c(vector_i$`estimate`, vector_i$`SE`)
+                     }
+                   }),
+            format = sprintf_format("%.3f (%.3f)")
+      ),
+      rrowl(paste0(mmrm_result$`conf_level`*100, "% CI"),
+            lapply(split(df2_i,
+                         factor(df2_i[[arm_var]], levels = arm_lvl),
+                         drop = FALSE),
+                   function(vector_i) {
+                     if (is.null(vector_i)) {
+                       NULL
+                     } else {
+                       c(vector_i$`lower.CL`, vector_i$`upper.CL`)
+                     }
+                   }),
+            format = "(xx.xxx, xx.xxx)"
+      ),
+      rrowl("Relative Reduction (%)",
+            lapply(split(df2_i, df2_i[[arm_var]], drop = FALSE),
+                   function(vector_i) {
+                     if (is.null(vector_i)) {
+                       NULL
+                     } else {
+                       c(vector_i$`relative_reduc`)
+                     }
+                   }),
+            format = "xx.x%"
+      ),
+      rrow(),
+      rrowl("p-value (MMRM)",
+            lapply(split(df2_i, df2_i[[arm_var]], drop = FALSE),
+                   function(vector_i) {
+                     if (is.null(vector_i)) {
+                       NULL
+                     } else {
+                       c(vector_i$`p.value`)
+                     }
+                   }),
+            format = format_pval
+      )
+    )
+
+    tbl <- header_add_N(tbl, col_N)
+
+    node(
+      name = visit,
+      content = tbl,
+      children = NULL
+    )
+
+  }, s_estimate_df, s_contrast_df, names(s_estimate_df))
+
+  tree <- invisible_node(
+    name = "root",
+    children = mmrm_node_list,
+    content = NULL
+  )
+
+  if (table_tree) {
+    tree
+  } else {
+    to_rtable(tree)
+  }
+}
+
+#' MMRM model, test, estimate
+#'
+#'
+#' @param formula a gls formula.
+#' @param data a \code{data.frame} with all the variable that are used in
+#'   \code{formula}. The arm column in data must be a factor.
+#' @param id_var a character describing the variable name used as subject IDs,
+#'   "USUBJID" by default.
+#' @param arm_var a character describing the variable name for \code{\link{arm}},
+#'   "ARM" by default. This variable must be factor in data.
+#' @param visit_var a character describing the variable name for visit,
+#'   "AVISIT" by default. This variable must be factor in data.
+#' @param mode algorithm for degree of freedom: "auto", "df.error" or
+#'   "boot-satterthwaite".
+#' @param conf.level confidence level. Must be number greater than 0 and less
+#'   than 1.
+#' @param weights_emmeans argument from emmeans, "proportional" by default.
+#' @param corStruct \code{NULL} by default or a string with the name of \link[nlme]{corClasses}.
+#'
+#' @return a dataframe with MMRM results
+#'
+#' @importFrom nlme gls corSymm corAR1 corARMA corCAR1 corCompSymm
+#'  corExp corGaus corLin corRatio corSpher varIdent
+#' @importFrom emmeans emmeans contrast
+#'
+#' @export
+#'
+#' @examples
+#' library(random.cdisc.data)
+#' library(dplyr)
+#'
+#' ADSL <- radsl(cached = TRUE)
+#' ADQS <- radqs(cached = TRUE)
+#' ADQS_f <- ADQS %>%
+#'   filter(PARAMCD == "FKSI-FWB" & ABLFL != "Y" & ABLFL2 != "Y") %>%
+#'   droplevels()
+#'
+#' mmrm_results <- a_mmrm(
+#'   data = ADQS_f,
+#'   formula = AVAL ~ ARM + AVISIT + STRATA1 + BMRKR2 + BASE + ARM * AVISIT,
+#'   id_var = "USUBJID",
+#'   arm_var = "ARM",
+#'   visit_var = "AVISIT",
+#'   mode = "boot-satterthwaite",
+#'   conf.level = 0.95,
+#'   weights_emmeans = "proportional",
+#'   corStruct = "corSymm"
+#' )
+#'
+#' names(mmrm_results)
+#'
+#' mmrm_results["contrast"]
+#' mmrm_results["estimate"]
+#'
+a_mmrm <- function(data,
+                   formula = AVAL ~ ARM + AVISIT + ARM * VISIT,
+                   id_var = "USUBJID",
+                   arm_var = "ARM",
+                   visit_var = "AVISIT",
+                   mode = c("df.error", "auto", "boot-satterthwaite"),
+                   conf.level = 0.95,
+                   weights_emmeans = "proportional",
+                   corStruct = NULL
+) {
+
+  mode <- match.arg(mode)
+
+  stopifnot(
+    is.data.frame(data),
+    id_var %in% names(data),
+    arm_var %in% names(data),
+    visit_var %in% names(data),
+    is.null(corStruct) || corStruct %in% c(
+      "corAR1", "corARMA", "corCAR1", "corCompSymm", "corExp", "corGaus",
+      "corLin", "corRatio", "corSpher", "corSymm"
+    )
+  )
+
+  # extract relevant information from formula
+  mt <- terms(formula, data = data)
+
+  vars <- all.vars(attr(mt, "variables")) # different than  all.vars(formula)?
+  i_resp <- attr(mt,"response")
+  if ( i_resp == 0) {
+    stop("need a response variable")
+  }
+
+  response_var <- vars[i_resp]
+
+  if (!all(vars %in% names(data))) {
+    stop("All variables in the formula  must appear in 'data' (no scoping allowed)")
+  }
+
+  environment(formula) <- new.env() # no scoping for formula elements needed
+
+  # names are strings and symbols can be substituted into bquote and used with !!
+  arm_symbol <- sym(arm_var)
+  arm_values <- data[[arm_var]]
+  response_values <- data[[response_var]]
+
+  stopifnot(nlevels(arm_values) >= 2)
+  reference_level <- levels(arm_values)[1]
+
+  stopifnot(
+    is.numeric(response_values),
+    is.factor(arm_values),
+    is.factor(data[[visit_var]])
+  )
+
+  # Modeling step in SAS assumes complete case; however estimation step considers all data
+  if (any(is.na(response_values))) {
+    warning(
+      "Some records have a missing endpoint, which will be excluded from MMRM modeling.",
+      head(data[is.na(response_values), c(id_var, visit_var, response_var)]),
+      call. = FALSE
+    )
+  }
+
+  # remove all entries where response is NA, droplevels as well
+  data_cc <- data %>%
+    filter(!is.na(!!sym(response_var))) %>%
+    droplevels()
+
+  # check all arms will still be present after NA filtering
+  stopifnot(nlevels(arm_values) == nlevels(data_cc[[arm_var]]))
+
+  # each arm should have at least have 5 records
+  if (!all(table(data_cc[[arm_var]]) > 5)) {
+    stop(paste("Each group / arm should have at least 5 records with non-missing", response_var))
+  }
+
+  cor_formula <- as.formula(paste("~", "as.numeric(", visit_var, ") | ", id_var))
+
+  correlation <- if (is.null(corStruct)) {
+    NULL
+  } else {
+    do.call(corStruct, list(form = cor_formula))
+  }
+
+  fit_mmrm <- gls(
+    model = formula,
+    correlation = correlation,
+    weights = varIdent(form = as.formula(paste(" ~ 1 |", visit_var))),
+    method = "REML",
+    data = data_cc, # model fit on complete case
+    na.action = na.exclude
+  )
+
+  # hacky for emmeans since emmeans evaluation and namespace is not good
+  # https://github.com/rvlenth/emmeans/issues/13
+  fit_mmrm$call$model <- substitute(formula)
+  emm <- emmeans(
+    fit_mmrm,
+    mode = mode,
+    specs = as.formula(paste("~ ", arm_var, "|", visit_var)),
+    data = data %>% select(-c(response_var)), # estimate on original data
+    weights = weights_emmeans
+  )
+
+  # Relative Reduction (in change from baseline) is calculated using model based
+  # LS means as 100*(LS mean change from baseline in Control Pooled group –
+  # LS mean change from baseline in Treatment Group)/LS mean change from
+  # baseline in Control Pooled group.
+
+  ### adjusted estimate for each arm
+  estimate <- confint(emm, level = conf.level) %>%
+    as.data.frame()
+
+  # add n information
+
+  data_n <- data %>%
+    group_by_at(.vars = c(visit_var, arm_var)) %>%
+    summarise(n = n())
+
+  estimate <- estimate %>%
+    left_join(., data_n, by = c(visit_var, arm_var))
+
+  # get emmean for reference group to join into full dataframe so that relative reduction in
+  # emmean (mean of response variable) can be computed with respect to reference level (e.g. ARM A)
+  means_at_ref <- estimate %>%
+    filter(!!arm_symbol == reference_level) %>%
+    select(c(visit_var, "emmean")) %>%
+    rename(ref = emmean)
+
+  relative_reduc <- estimate %>%
+    filter(!!arm_symbol != reference_level) %>%
+    left_join(means_at_ref, by = c(visit_var)) %>%
+    mutate(relative_reduc = (ref - emmean) / ref) %>%
+    select(c(visit_var, arm_var, "relative_reduc"))
+
+  sum_fit_diff <- summary(contrast(emm, method = "trt.vs.ctrl"), level = conf.level, infer = c(TRUE, TRUE), adjust = "none")
+
+  # get the comparison group name from "contrast" column, e.g. "ARMB - ARMA" returns "ARMB", i.e. remove " - ARMA"
+  contrast <- sum_fit_diff %>%
+    mutate(
+      col_by = gsub(paste0("\\s-\\s", reference_level), "", contrast) %>%
+        factor(., levels = levels(data[[arm_var]]))
+    ) %>%
+    select(-contrast) %>%
+    rename(!!arm_symbol := col_by) %>%
+    left_join(., relative_reduc, by = c(visit_var, arm_var))
+
+  list(
+    contrast = contrast,
+    estimate = estimate,
+    ref_level = reference_level,
+    conf_level = conf.level
+  )
+
+}
--- README.md
+++ README.md
@@ -1,102 +1,26 @@
- 
--   [tern](#tern)
--   [Installation](#installation)
-    -   [Stable Version](#stable-version)
-    -   [Development Version](#development-version)
--   [Development](#development)
-    -   [Issues](#issues)
-    -   [Collaboration](#collaboration)
-        -   [Creating Pull Requests](#creating-pull-requests)
--   [Presentations](#presentations)
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-tern
-====
+# tern
 
-The `tern` R package contains analysis functions to create tables, listings and graphs (TLGs) as used in clinical trials. We also provide [teal](https://github.roche.com/NEST/teal) modules for outputs in `tern` in the [teal.modules.clinical](https://github.roche.com/NEST/teal.modules.clinical) R package.
+The `tern` R package contains analysis functions to create tables,
+listings and graphs (TLGs) as used in clinical trials. We also provide
+[teal](https://github.roche.com/NEST/teal) modules for outputs in `tern`
+in the
+[teal.modules.clinical](https://github.roche.com/NEST/teal.modules.clinical)
+R package.
 
--   Note that currently **tern may not be used for regulatory submissions**
+  - Note that currently **tern may not be used for regulatory
+    submissions**
 
--   However you can use `tern` for exploratory analysis and for quality control
+  - However you can use `tern` for exploratory analysis and for quality
+    control
 
-For the documentation please refer to the official [documentation webpage](https://pages.github.roche.com/NEST/docs/).
+# Installation
 
-Installation
-============
+See the [agile-R website](https://go.roche.com/agile-R)
 
-Stable Version
---------------
+# Presentations
 
-``` r
-devtools::install_github(
-  repo = "NEST/random.cdisc.data",
-  ref = "master", 
-  host = "https://github.roche.com/api/v3",
-  upgrade_dependencies = FALSE
-)
-
-devtools::install_github("Roche/rtables")
-
-devtools::install_github(
-  repo = "NEST/tern",
-  ref = "master", 
-  host = "https://github.roche.com/api/v3",
-  upgrade_dependencies = FALSE
-)
-```
-
-Development Version
--------------------
-
-To install the current development version of `tern` run:
-
-``` r
-devtools::install_github(
-  repo = "NEST/random.cdisc.data",
-  ref = "devel", 
-  host = "https://github.roche.com/api/v3",
-  upgrade_dependencies = FALSE
-)
-
-devtools::install_github("Roche/rtables", ref = "devel", upgrade_dependencies = FALSE)
-
-devtools::install_github(
-  repo = "NEST/tern",
-  ref = "devel", 
-  host = "https://github.roche.com/api/v3",
-  upgrade_dependencies = FALSE
-)
-```
-
-Development
-===========
-
-Issues
-------
-
-We hope that `tern` is useful for your everyday work. If you would like more TLGs or you need more configuration options for the existing functions then please fill out a [`github` issue](https://github.roche.com/NEST/tern/issues) with your request and the reason/context for your request.
-
--   Please do not submit issues with ideas for useful features for current `tern` functions if you do not necessarily need them for your project. We are aware that many of `tern`'s functions would benefit from additional arguments in order to control the outputs in more detail. However, we would like to avoid argument explosion and one way to do that is to get more experience with `tern` *in the field* and then slowly grow the flexibility based on need.
-
--   Please do not paste outputs with sensitive information into the issues. If possible make a minimal working example with `random.cdisc.data` data.
-
-Collaboration
--------------
-
-We would like `tern` to be a collaborative effort and your contributions are welcome! However please read the following points before you make any contributions:
-
--   talk to the respective function authors before you start working on changes other than bug fixes or documentation examples (this includes argument changes)
-
-    -   please have a look at the [R code](https://github.roche.com/NEST/tern/tree/master/R) of a couple of functions in `tern`. If you are new to `R` or do not want to spend the effort to create similarly general and robust functions then please consider contributing to the [osprey](https://github.roche.com/Rpackages/osprey) R package.
-
-### Creating Pull Requests
-
--   Please make pull requests to the `master` branch (i.e. base is master) 
-
--   We then make new tagged releases on the master branch once the `master` branch has been thoroughly tested
-
-Presentations
-=============
-
--   [Stats Software Initiative - R Series. June 18, 2018](https://docs.google.com/presentation/d/1OB7MMt3YKzfMJ-gXcGpcRqM8tjbMZWqeEki164L38i4/edit?usp=sharing)
+  - [Stats Software Initiative - R Series.
+    June 18, 2018](https://docs.google.com/presentation/d/1OB7MMt3YKzfMJ-gXcGpcRqM8tjbMZWqeEki164L38i4/edit?usp=sharing)
