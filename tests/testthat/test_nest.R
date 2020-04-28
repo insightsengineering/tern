7184072dd6552fed8055aa026a3fb265456b95fb
Wang, Heng {MDBB~South San Francisco} <wang.heng@gene.com>
2020-04-28 16:47:46 -0700

    365 t tte testcase (#367)
    * add test for t_tte
    
    * re-format
    
    * minor
    
    * 120char
    
    * minor update
    
    * minor
    
    * updates to address code formatting related chk failures

--- tests/testthat/test_nest.R
+++ tests/testthat/test_nest.R
@@ -5,16 +5,31 @@ test_strict(
   exclude_from_man = c(
     # survival package
     # simpleWarning: partial match of 'std' to 'std.err'
-    "t_forest_tte.Rd",
-    "t_el_forest_tte.Rd",
+    "a_mmrm.Rd",
+    "basic_node_info.Rd",
+    "df_explicit_na.Rd",
+    "displayable.Rd",
+    "full_apply_at_depth.Rd",
     "g_forest.Rd",
-    "t_km.Rd",
     "g_km.Rd",
+    "invisible_node.Rd",
+    "node-class.Rd",
+    "node_format_data.Rd",
+    "rapply_tree.Rd",
+    "rsort_tree.Rd",
+    "s_coxph_pairwise.Rd",
+    "sub-sub-node-method.Rd",
+    "t_coxph.Rd",
+    "t_el_disposition.Rd",
+    "t_el_forest_rsp.Rd",
+    "t_el_forest_tte.Rd",
+    "t_forest_tte.Rd",
+    "t_km.Rd",
     "t_tte.Rd",
+    "tabulate_pairwise.Rd",
+    "to_rtable.Rd"
     # simpleWarning in model.matrix.default(Terms, mf, contrasts = contrast.arg): partial argument match of 'contrasts'
     # to 'contrasts.arg'
-    "s_coxph_pairwise.Rd",
-    "t_coxph.Rd"
   )
 )
 test_regexp()
