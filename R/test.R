035b0b393c740d6a8feeb61e7f943b1ec8c21e2a
Waddell, Adrian {MDBB~Basel} <adrian.waddell@roche.com>
2020-08-07 13:59:41 +0200

    add a dummy function to test automation (#531)

---
+++ R/test.R
@@ -0,0 +1,20 @@
+
+
+#' dummy function to test automation
+#'
+#'
+#' yet some more text
+#'
+#' @export
+#'
+#' @param x something
+#'
+#'
+#' @examples
+#'
+#' test()
+#'
+#' print("AAA")
+test <- function(x) {
+  print("Hello World")
+}
