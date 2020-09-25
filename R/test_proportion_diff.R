1ace6fcf377dc0fb902c55a05d1577fd9465897b
Collin, Francois {FISV~Warsaw} <francois.collin@contractors.roche.com>
2020-09-25 14:06:30 +0200

    New prop_fisher, prop_schouten

--- R/test_proportion_diff.R
+++ R/test_proportion_diff.R
@@ -3,8 +3,12 @@
 #' Various tests were implemented to test the difference between two
 #' proportions.
 #'
-#' @template formatting_arguments
+#' @param tbl (`matrix`)\cr
+#'   with two groups in rows and the binary response (`TRUE`/`FALSE`) in
+#'   columns.
 #'
+#' @template formatting_arguments
+#' @order 1
 #' @name prop_diff_test
 #'
 NULL
@@ -12,12 +16,14 @@ NULL
 
 #' @describeIn prop_diff_test performs Chi-Squared test.
 #'   Internally calls [stats::prop.test()].
-#' @param tbl (`matrix`)\cr
-#'   with two groups in rows and the binary response (`TRUE`/`FALSE`) in
-#'   columns.
 #' @importFrom stats prop.test
 #' @export
+#' @order 2
 #' @examples
+#'
+#' # Non-stratified proportion difference test
+#'
+#' ## Data
 #' A <- 20
 #' B <- 20
 #' set.seed(1)
@@ -27,31 +33,96 @@ NULL
 #' )
 #' grp <- c(rep("A", A), rep("B", B))
 #' tbl <- table(grp, rsp)
+#'
+#' ## Chi-Squared test
 #' prop_chisq(tbl)
 #'
-prop_chisq <- function(tbl) stats::prop.test(tbl, correct = FALSE)$p.value
+prop_chisq <- function(tbl) {
+  assert_that(ncol(tbl) == 2, nrow(tbl) == 2)
+  stats::prop.test(tbl, correct = FALSE)$p.value
+}
 
 
 #' @describeIn prop_diff_test performs stratified Cochran-Mantel-Haenszel test.
 #'   Internally calls [stats::mantelhaen.test()].
-#' @param tbl (`array`)\cr
+#' @param ary (`array`, 3 dimensions)\cr
 #'   with two groups in rows, the binary response (`TRUE`/`FALSE`) in
-#'   columns, the strata in third dimension.
+#'   columns, the strata in the third dimension.
 #' @importFrom stats mantelhaen.test
 #' @export
+#' @order 3
 #' @examples
 #'
+#' # Stratified proportion difference test
+#'
+#' ## Data
 #' rsp <- sample(c(TRUE, FALSE), 100, TRUE)
 #' grp <- factor(rep(c("A", "B"), each = 50))
 #' strata <- factor(rep(c("V", "W", "X", "Y", "Z"), each = 20))
 #' tbl <- table(grp, rsp, strata)
+#'
+#' ## Cochran-Mantel-Haenszel test
 #' prop_cmh(tbl)
 #'
-prop_cmh <- function(tbl) {
+prop_cmh <- function(ary) {
 
-  if (any(apply(tbl, MARGIN = 3, sum) < 5)) {
+  assert_that(
+    is.array(ary),
+    ncol(ary) == 2, nrow(ary) == 2,
+    length(dim(ary)) == 3
+  )
+
+  if (any(apply(ary, MARGIN = 3, sum) < 5)) {
     warning("<5 data points in some strata. CMH test may be incorrect.")
   }
 
-  stats::mantelhaen.test(tbl, correct = FALSE)$p.value
+  stats::mantelhaen.test(ary, correct = FALSE)$p.value
+}
+
+
+#' @describeIn prop_diff_test performs the Chi-Squared test with Schouten
+#'   correction.
+#' @export
+#' @order 2
+#' @examples
+#'
+#' ## Chi-Squared test + Schouten correction.
+#' prop_schouten(tbl)
+#'
+prop_schouten <- function(tbl) {
+
+  assert_that(ncol(tbl) == 2, nrow(tbl) == 2)
+
+  # Source: STREAM v2
+  #nolint start
+  # https://github.roche.com/MDIS/stream2/blob/82c6c54ea6c61d11746af3b413ad6b1b213096cd/app/macro/str_tlg_method_resp.sas#L1623
+  #nolint end
+  count_1_1 <- tbl[1, "FALSE"]
+  count_1_2 <- tbl[1, "TRUE"]
+  count_2_1 <- tbl[2, "FALSE"]
+  count_2_2 <- tbl[2, "TRUE"]
+
+  t_schouten <- (count_1_1 + count_1_2 + count_2_1 + count_2_2 - 1) *
+    (abs(count_2_2 * count_1_1 - count_1_2 * count_2_1) -
+       0.5 * min(count_1_1 + count_1_2, count_2_1 + count_2_2))^2 /
+    ((count_1_1 + count_1_2) * (count_2_1 + count_2_2) *
+       (count_1_2 + count_2_2) * (count_1_1 + count_2_1))
+
+  1 - stats::pchisq(t_schouten, df = 1)
+}
+
+
+#' @describeIn prop_diff_test performs the Fisher's exact test.
+#'   Internally calls [stats::fisher.test()].
+#' @importFrom stats fisher.test
+#' @export
+#' @order 2
+#' @examples
+#'
+#' ## Fisher's exact test
+#' prop_fisher(tbl)
+#'
+prop_fisher <- function(tbl) {
+  assert_that(ncol(tbl) == 2, nrow(tbl) == 2)
+  stats::fisher.test(tbl)$p.value
 }
--- tests/testthat/test-test_proportion_diff.R
+++ tests/testthat/test-test_proportion_diff.R
@@ -15,7 +15,6 @@ test_that("prop_chisq returns right result", {
 
 })
 
-
 test_that("prop_cmh returns right result", {
 
   set.seed(1)
@@ -26,6 +25,39 @@ test_that("prop_cmh returns right result", {
 
   result <- prop_cmh(tbl)
   expected <- 0.6477
+  expect_equal(result, expected, tolerance = 1e-4, check.attributes = FALSE)
+
+})
+
+test_that("prop_fisher returns right result", {
+
+  set.seed(1)
+  rsp <- c(
+    sample(c(TRUE, FALSE), size = 20, prob = c(3 / 4, 1 / 4), replace = TRUE),
+    sample(c(TRUE, FALSE), size = 20, prob = c(1 / 2, 1 / 2), replace = TRUE)
+  )
+  grp <- c(rep("A", 20), rep("B", 20))
+  tbl <- table(grp, rsp)
+
+  result <- prop_fisher(tbl)
+  expected <- 0.1110
+  expect_equal(result, expected, tolerance = 1e-4, check.attributes = FALSE)
+
+})
+
+
+test_that("prop_schouten returns right result", {
+
+  set.seed(1)
+  rsp <- c(
+    sample(c(TRUE, FALSE), size = 20, prob = c(3 / 4, 1 / 4), replace = TRUE),
+    sample(c(TRUE, FALSE), size = 20, prob = c(1 / 2, 1 / 2), replace = TRUE)
+  )
+  grp <- c(rep("A", 20), rep("B", 20))
+  tbl <- table(grp, rsp)
+
+  result <- prop_schouten(tbl)
+  expected <- 0.0843
   expect_equal(result, expected, tolerance = 1e-4, check.attributes = FALSE)
 
 })
