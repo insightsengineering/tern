d12204ee062a700bf494215d3fad4b8d59f958e2
Chohan, Saibah {MDBA~Mississauga} <saibah.chohan@roche.com>
2020-10-07 03:41:14 -0400

    682 add test for vst01 (#724)

---
+++ tests/testthat/test-table_vst01.R
@@ -0,0 +1,126 @@
+# Test the single variant for VST01
+
+library(random.cdisc.data)
+library(rtables)
+library(dplyr)
+
+gen_advs <- function(){
+  advs <- radvs(cached = TRUE)
+  advs_label <- var_labels(advs)
+
+  advs <- advs %>%
+    filter(PARAMCD == "DIABP" & PARAM == "Diastolic Blood Pressure") %>%
+    mutate(PARAMCD = droplevels(PARAMCD), PARAM = droplevels(PARAM)) #nolint
+
+  # post-baseline
+  advs_pb <- advs %>% filter(ABLFL != "Y" & ABLFL2 != "Y") #nolint
+
+  advs_pb_max <- advs_pb %>%
+    group_by(PARAM, USUBJID) %>%
+    arrange(desc(AVAL)) %>%
+    slice(1) %>%
+    ungroup() %>%
+    mutate(AVISIT = "Post-Baseline Maximum")
+
+  advs_pb_min <- advs_pb %>%
+    group_by(PARAM, USUBJID) %>%
+    arrange(AVAL) %>%
+    slice(1) %>%
+    ungroup() %>%
+    mutate(AVISIT = "Post-Baseline Minimum")
+
+  advs_pb_last <- advs_pb %>%
+    group_by(PARAM, USUBJID) %>%
+    arrange(desc(AVISITN)) %>%
+    slice(1) %>%
+    ungroup() %>%
+    mutate(AVISIT = "Post-Baseline Last")
+
+  advs_f <- rbind(
+    advs,
+    advs_pb_last,
+    advs_pb_min,
+    advs_pb_max
+  )
+
+  var_labels(advs_f) <- advs_label
+  advs_f <- advs_f %>% mutate(AVISIT = droplevels(AVISIT))  #nolint
+  advs_f
+}
+
+test_that("VST01 default variant is produced correctly", {
+  advs <- gen_advs()
+  advs_baseline <- advs %>% filter(ABLFL == "Y")   #nolint
+  n_col_counts <- rep(table(advs_baseline$ARM), each = 2)
+
+  result <- basic_table() %>%
+    split_cols_by("ARM") %>%
+    add_colcounts() %>%
+    split_rows_by("AVISIT") %>%
+    split_cols_by_multivar(
+      vars = c("AVAL", "CHG"),
+      varlabels = c("Value at Visit", "Change from Baseline")) %>%
+    summarize_colvars(.labels = c(range = "Min - Max")) %>%
+    build_table(df = advs, col_counts = n_col_counts)
+
+  result_matrix <- to_string_matrix(result)
+  expected_matrix <- structure(c(
+    "", "", "", "SCREENING", "n", "Mean (SD)", "Median",
+    "Min - Max", "BASELINE", "n", "Mean (SD)", "Median", "Min - Max",
+    "WEEK 1 DAY 8", "n", "Mean (SD)", "Median", "Min - Max", "WEEK 2 DAY 15",
+    "n", "Mean (SD)", "Median", "Min - Max", "WEEK 3 DAY 22", "n",
+    "Mean (SD)", "Median", "Min - Max", "WEEK 4 DAY 29", "n", "Mean (SD)",
+    "Median", "Min - Max", "WEEK 5 DAY 36", "n", "Mean (SD)", "Median",
+    "Min - Max", "Post-Baseline Last", "n", "Mean (SD)", "Median",
+    "Min - Max", "Post-Baseline Minimum", "n", "Mean (SD)", "Median",
+    "Min - Max", "Post-Baseline Maximum", "n", "Mean (SD)", "Median",
+    "Min - Max", "A: Drug X", "Value at Visit", "(N=134)", "", "134",
+    "50 (7.2)", "49.7", "31.7 - 71.2", "", "134", "48.6 (8)", "48.4",
+    "27.7 - 64.6", "", "134", "50.3 (7.5)", "50.1", "33 - 69", "",
+    "134", "50.8 (7.8)", "51.4", "31.9 - 70.3", "", "134", "50.7 (7.8)",
+    "50.2", "29.1 - 74.9", "", "134", "50.1 (8.1)", "49.3", "29.4 - 71.4",
+    "", "134", "50.6 (7.5)", "49.4", "35.1 - 72.2", "", "134", "50.6 (7.5)",
+    "49.4", "35.1 - 72.2", "", "134", "41.6 (4.8)", "42.1", "29.1 - 54.2",
+    "", "134", "60 (4.9)", "59.7", "47.6 - 74.9", "A: Drug X", "Change from Baseline",
+    "(N=134)", "", "0", "NA (NA)", "NA", "NA - NA", "", "134", "0 (0)",
+    "0", "0 - 0", "", "134", "1.7 (10.8)", "0.6", "-20.4 - 29.9",
+    "", "134", "2.2 (11.8)", "3.1", "-23.9 - 39.2", "", "134", "2.1 (10.9)",
+    "2.6", "-25.8 - 30.7", "", "134", "1.5 (10.9)", "1.2", "-30.9 - 24.8",
+    "", "134", "2 (11.2)", "1.6", "-25.2 - 31.5", "", "134", "2 (11.2)",
+    "1.6", "-25.2 - 31.5", "", "134", "-7 (9.6)", "-5.6", "-30.9 - 13.2",
+    "", "134", "11.4 (9.3)", "11.7", "-8.6 - 39.2", "B: Placebo",
+    "Value at Visit", "(N=134)", "", "134", "50.8 (8.4)", "50.1",
+    "29.3 - 69.2", "", "134", "50.4 (7.9)", "50.2", "21.7 - 67.5",
+    "", "134", "49.7 (7.7)", "49.7", "33.7 - 66.5", "", "134", "49.7 (8.4)",
+    "50.2", "30.6 - 68.1", "", "134", "49.1 (7.9)", "49.5", "24.5 - 67.1",
+    "", "134", "49.6 (7.1)", "49.2", "32.7 - 67", "", "134", "48.4 (8)",
+    "47.7", "30.4 - 70.6", "", "134", "48.4 (8)", "47.7", "30.4 - 70.6",
+    "", "134", "40.3 (5.3)", "40.3", "24.5 - 55.4", "", "134", "58.5 (4.8)",
+    "58.3", "47.8 - 70.6", "B: Placebo", "Change from Baseline",
+    "(N=134)", "", "0", "NA (NA)", "NA", "NA - NA", "", "134", "0 (0)",
+    "0", "0 - 0", "", "134", "-0.8 (10.9)", "-1.1", "-25.8 - 28.4",
+    "", "134", "-0.7 (12.4)", "-0.7", "-28.4 - 37", "", "134", "-1.3 (11.1)",
+    "-2.3", "-28.1 - 33.9", "", "134", "-0.8 (10.6)", "-0.8", "-33.2 - 35.9",
+    "", "134", "-2.1 (11.7)", "-2.9", "-31.2 - 29.2", "", "134",
+    "-2.1 (11.7)", "-2.9", "-31.2 - 29.2", "", "134", "-10.1 (10.2)",
+    "-10.7", "-33.2 - 27.7", "", "134", "8.1 (9.5)", "7.5", "-14 - 37",
+    "C: Combination", "Value at Visit", "(N=132)", "", "132", "50.2 (7.6)",
+    "49.6", "26.9 - 70", "", "132", "51.1 (7.8)", "50.8", "29.7 - 71.4",
+    "", "132", "48.9 (7.9)", "47.7", "30.4 - 67", "", "132", "50 (8.3)",
+    "51", "24.8 - 65.6", "", "132", "49.9 (7.7)", "50", "31.5 - 68.5",
+    "", "132", "49.7 (8)", "49.8", "28.7 - 68.4", "", "132", "49.1 (7.6)",
+    "49.2", "30.6 - 72.9", "", "132", "49.1 (7.6)", "49.2", "30.6 - 72.9",
+    "", "132", "40.5 (5.3)", "40.5", "24.8 - 54.9", "", "132", "58.6 (4.8)",
+    "59", "41.8 - 72.9", "C: Combination", "Change from Baseline",
+    "(N=132)", "", "0", "NA (NA)", "NA", "NA - NA", "", "132", "0 (0)",
+    "0", "0 - 0", "", "132", "-2.3 (10.4)", "-2.2", "-22.1 - 22.4",
+    "", "132", "-1.1 (11.6)", "-2.3", "-29.2 - 30.7", "", "132",
+    "-1.2 (10.9)", "-0.6", "-29.7 - 27", "", "132", "-1.4 (11)",
+    "-0.2", "-35.5 - 28.4", "", "132", "-2 (10.7)", "-2.2", "-30.5 - 34.6",
+    "", "132", "-2 (10.7)", "-2.2", "-30.5 - 34.6", "", "132", "-10.6 (9.1)",
+    "-10.9", "-35.5 - 13.7", "", "132", "7.5 (9.1)", "6.8", "-17.3 - 34.6"
+  ), .Dim = c(53L, 7L), .Dimnames = list(NULL, c(
+    "", "A: Drug X", "A: Drug X", "B: Placebo", "B: Placebo", "C: Combination", "C: Combination"
+  )))
+  expect_identical(result_matrix, expected_matrix)
+})
