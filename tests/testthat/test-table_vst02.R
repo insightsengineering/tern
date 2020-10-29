02cb25a0c2e6f894f71cbb39b3ae7b7c065f7234
Collin, Francois {FISV~Warsaw} <francois.collin@contractors.roche.com>
2020-10-29 15:09:23 +0100

    807 vst02 2 table test (#809)

--- tests/testthat/test-table_vst02.R
+++ tests/testthat/test-table_vst02.R
@@ -3,7 +3,7 @@
 library(random.cdisc.data)
 library(dplyr)
 
-test_that("VST02 default variant is produced correctly", {
+test_that("1. Vital Sign Abnormalities (Regardless of Abnormality at Baseline, VST02_1)", {
   adsl <- radsl(cached = TRUE)
   advs <- radvs(cached = TRUE)
 
@@ -41,5 +41,52 @@ test_that("VST02 default variant is produced correctly", {
     "159/400 (39.8%)", "", "170/400 (42.5%)", "151/400 (37.8%)",
     "", "182/400 (45.5%)", "167/400 (41.8%)"),
     .Dim = c(21L, 5L))
+  expect_identical(result_matrix, expected_matrix)
+})
+
+test_that("2. Vital Sign Abnormalities (Among Subject Without Abnormality at Baseline, VST02_2)", {
+
+  adsl <- radsl(cached = TRUE)
+  advs <- radvs(cached = TRUE)
+
+  advs_f <- advs %>% filter(AVISITN >= 0)
+  n_col_counts <- table(adsl$ARM)
+  n_col_counts <- c(n_col_counts, sum(n_col_counts))
+
+  result <- basic_table() %>%
+    split_cols_by("ARM", split_fun = add_overall_level("All Patients", first = FALSE)) %>%
+    add_colcounts() %>%
+    split_rows_by("PARAM", split_label = c("Parameter / Abnormality Direction"), visible_label = TRUE) %>%
+    count_abnormal("ANRIND", abnormal = c(Low = "LOW", High = "HIGH")) %>%
+    build_table(df = advs_f, col_counts = n_col_counts)
+
+  result_matrix <- to_string_matrix(result)
+  dput(result_matrix)
+
+  expected_matrix <- structure(
+    c(
+      "", "", "Parameter / Abnormality Direction", "Diastolic Blood Pressure",
+      "Low", "High", "Pulse Rate", "Low", "High", "Respiratory Rate",
+      "Low", "High", "Systolic Blood Pressure", "Low", "High", "Temperature",
+      "Low", "High", "Weight", "Low", "High", "A: Drug X", "(N=134)",
+      "", "", "45/120 (37.5%)", "47/113 (41.6%)", "", "60/125 (48%)",
+      "47/119 (39.5%)", "", "53/123 (43.1%)", "57/124 (46%)", "", "47/116 (40.5%)",
+      "55/121 (45.5%)", "", "47/119 (39.5%)", "43/128 (33.6%)", "",
+      "54/120 (45%)", "49/123 (39.8%)", "B: Placebo", "(N=134)", "",
+      "", "53/121 (43.8%)", "48/123 (39%)", "", "49/121 (40.5%)", "52/119 (43.7%)",
+      "", "45/118 (38.1%)", "52/127 (40.9%)", "", "51/120 (42.5%)",
+      "43/125 (34.4%)", "", "44/119 (37%)", "47/121 (38.8%)", "", "50/117 (42.7%)",
+      "54/121 (44.6%)", "C: Combination", "(N=132)", "", "", "48/118 (40.7%)",
+      "48/119 (40.3%)", "", "47/118 (39.8%)", "34/117 (29.1%)", "",
+      "39/118 (33.1%)", "53/118 (44.9%)", "", "51/120 (42.5%)", "49/124 (39.5%)",
+      "", "55/115 (47.8%)", "50/122 (41%)", "", "57/119 (47.9%)", "47/113 (41.6%)",
+      "All Patients", "(N=400)", "", "", "146/359 (40.7%)", "143/355 (40.3%)",
+      "", "156/364 (42.9%)", "133/355 (37.5%)", "", "137/359 (38.2%)",
+      "162/369 (43.9%)", "", "149/356 (41.9%)", "147/370 (39.7%)",
+      "", "146/353 (41.4%)", "140/371 (37.7%)", "", "161/356 (45.2%)",
+      "150/357 (42%)"
+    ),
+    .Dim = c(21L, 5L)
+  )
   expect_identical(result_matrix, expected_matrix)
 })
