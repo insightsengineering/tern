7c903ad73d6c28144aaa26a7cea5944ca3d7805d
Stoilova, Jana {MDBB~Mississauga} <jana.stoilova@roche.com>
2020-11-06 03:43:09 -0500

    716: Update table test for ONCT05. (#829)

--- tests/testthat/test-table_onct05.R
+++ tests/testthat/test-table_onct05.R
@@ -39,24 +39,24 @@ test_that("ONCT05 variant 1 (Objective Response Rate by Subgroup) is produced co
     tabulate_rsp_subgroups(vars = c("n_tot", "or", "ci"), conf_level = 0.95) %>%
     build_table(df$or)
 
-  result <- cbind_rtables(t1, t2)
+  result <- cbind_rtables(t2[, 1], t1, t2[, -1])
 
   result_matrix <- to_string_matrix(result)
 
   expected_matrix <- structure(
     c(
       "", "", "All Patients", "Sex", "F", "M", "Stratification Factor 2",
-      "S1", "S2", "B: Placebo", "n", "100", "", "62", "38", "", "48",
-      "52", "B: Placebo", "Response (%)", "71%", "", "64.5%", "81.6%",
-      "", "70.8%", "71.2%", "A: Drug X", "n", "100", "", "58", "42",
-      "", "57", "43", "A: Drug X", "Response (%)", "90%", "", "91.4%",
-      "88.1%", "", "89.5%", "90.7%", " ", "Total n", "200", "", "120",
-      "80", "", "105", "95", " ", "Odds Ratio", "3.68", "", "5.83",
-      "1.67", "", "3.50", "3.95", " ", "95% CI", "(1.68, 8.04)", "",
-      "(2.03, 16.73)", "(0.48, 5.79)", "", "(1.22, 10.00)", "(1.20, 13.01)"
+      "S1", "S2", " ", "Total n", "200", "", "120", "80", "", "105",
+      "95", "B: Placebo", "n", "100", "", "62", "38", "", "48", "52",
+      "B: Placebo", "Response (%)", "71%", "", "64.5%", "81.6%", "",
+      "70.8%", "71.2%", "A: Drug X", "n", "100", "", "58", "42", "",
+      "57", "43", "A: Drug X", "Response (%)", "90%", "", "91.4%",
+      "88.1%", "", "89.5%", "90.7%", " ", "Odds Ratio", "3.68", "",
+      "5.83", "1.67", "", "3.50", "3.95", " ", "95% CI", "(1.68, 8.04)",
+      "", "(2.03, 16.73)", "(0.48, 5.79)", "", "(1.22, 10.00)", "(1.20, 13.01)"
       ),
     .Dim = 9:8
-    )
+  )
   expect_equal(result_matrix, expected_matrix)
 
 })
@@ -89,19 +89,19 @@ test_that("ONCT05 variant 2 (Specifying class variables) is produced correctly",
     tabulate_rsp_subgroups(vars = c("n_tot", "or", "ci"), conf_level = 0.95) %>%
     build_table(df$or)
 
-  result <- cbind_rtables(t1, t2)
+  result <- cbind_rtables(t2[, 1], t1, t2[, -1])
 
   result_matrix <- to_string_matrix(result)
 
   expected_matrix <- structure(
     c(
       "", "", "All Patients", "Sex", "M", "F", "Stratification Factor 1",
-      "C", "B", "A", "B: Placebo", "n", "100", "", "38", "62", "",
-      "36", "35", "29", "B: Placebo", "Response (%)", "71%", "", "81.6%",
+      "C", "B", "A", " ", "Total n", "200", "", "80", "120", "", "72",
+      "71", "57", "B: Placebo", "n", "100", "", "38", "62", "", "36",
+      "35", "29", "B: Placebo", "Response (%)", "71%", "", "81.6%",
       "64.5%", "", "72.2%", "74.3%", "65.5%", "A: Drug X", "n", "100",
       "", "42", "58", "", "36", "36", "28", "A: Drug X", "Response (%)",
       "90%", "", "88.1%", "91.4%", "", "94.4%", "77.8%", "100%", " ",
-      "Total n", "200", "", "80", "120", "", "72", "71", "57", " ",
       "Odds Ratio", "3.68", "", "1.67", "5.83", "", "6.54", "1.21",
       ">999.99", " ", "95% CI", "(1.68, 8.04)", "", "(0.48, 5.79)",
       "(2.03, 16.73)", "", "(1.32, 32.44)", "(0.41, 3.61)", "(0.00, >999.99)"
@@ -172,24 +172,25 @@ test_that("ONCT05 variant 4 (setting values indicating response) is produced cor
     tabulate_rsp_subgroups(vars = c("n_tot", "or", "ci"), conf_level = 0.95) %>%
     build_table(df$or)
 
-  result <- cbind_rtables(t1, t2)
+  result <- cbind_rtables(t2[, 1], t1, t2[, -1])
 
   result_matrix <- to_string_matrix(result)
 
   expected_matrix <- structure(
     c(
       "", "", "All Patients", "Sex", "F", "M", "Stratification Factor 2",
-      "S1", "S2", "B: Placebo", "n", "100", "", "62", "38", "", "48",
-      "52", "B: Placebo", "Response (%)", "95%", "", "96.8%", "92.1%",
-      "", "97.9%", "92.3%", "A: Drug X", "n", "100", "", "58", "42",
-      "", "57", "43", "A: Drug X", "Response (%)", "99%", "", "98.3%",
-      "100%", "", "100%", "97.7%", " ", "Total n", "200", "", "120",
-      "80", "", "105", "95", " ", "Odds Ratio", "5.21", "", "1.90",
+      "S1", "S2", " ", "Total n", "200", "", "120", "80", "", "105",
+      "95", "B: Placebo", "n", "100", "", "62", "38", "", "48", "52",
+      "B: Placebo", "Response (%)", "95%", "", "96.8%", "92.1%", "",
+      "97.9%", "92.3%", "A: Drug X", "n", "100", "", "58", "42", "",
+      "57", "43", "A: Drug X", "Response (%)", "99%", "", "98.3%",
+      "100%", "", "100%", "97.7%", " ", "Odds Ratio", "5.21", "", "1.90",
       ">999.99", "", ">999.99", "3.50", " ", "95% CI", "(0.60, 45.43)",
-      "", "(0.17, 21.53)", "(0.00, >999.99)", "", "(0.00, >999.99)", "(0.38, 32.55)"
+      "", "(0.17, 21.53)", "(0.00, >999.99)", "", "(0.00, >999.99)",
+      "(0.38, 32.55)"
       ),
     .Dim = 9:8
-    )
+  )
   expect_equal(result_matrix, expected_matrix)
 
 })
