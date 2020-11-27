d065b2096bb0698aded0814798300a1829ce8b96
Stoilova, Jana {MDBB~Mississauga} <jana.stoilova@roche.com>
2020-11-27 10:01:20 -0500

    Add  count_patients_with_flags. (#895)

--- tests/testthat/test-count_patients_with_event.R
+++ tests/testthat/test-count_patients_with_event.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 test_that("s_count_patients_with_event handles NA", {
 
   test_data <- data.frame(
@@ -81,7 +83,7 @@ test_that("count_patients_with_event works as expected for different column coun
 
   test_data <- data.frame(
     SUBJID = c("1001", "1001", "1001", "1002", "1002", "1002", "1003", "1003", "1003"),
-    ARM = c("A", "A", "A", "A", "A", "A", "B", "B", "B"),
+    ARM = factor(c("A", "A", "A", "A", "A", "A", "B", "B", "B"), levels = c("A", "B")),
     TRTEMFL = c("Y", "", "", "NA", "", "", "Y", "", ""),
     AEOUT = c("", "", "", "", "", "", "FATAL", "", "FATAL"),
     stringsAsFactors = FALSE
@@ -89,11 +91,13 @@ test_that("count_patients_with_event works as expected for different column coun
 
   test_adsl_like <- data.frame(
     SUBJID = as.character(1001:1010),
-    ARM = c("A", "A", "B", "B", "A", "A", "A", "B", "B", "A"),
+    ARM = factor(c("A", "A", "B", "B", "A", "A", "A", "B", "B", "A"), levels = c("A", "B")),
     stringsAsFactors = FALSE
   )
+  col_counts <- table(test_adsl_like$ARM)
 
-  l <- split_cols_by(lyt = NULL, var = "ARM") %>%
+  lyt <- basic_table() %>%
+    split_cols_by(var = "ARM") %>%
     count_patients_with_event(
       vars = "SUBJID",
       filters = c("TRTEMFL" = "Y"),
@@ -107,7 +111,7 @@ test_that("count_patients_with_event works as expected for different column coun
       denom = "N_col"
     )
 
-  result <- build_table(l, test_data, col_count = table(test_adsl_like$ARM))
+  result <- build_table(lyt, df = test_data, col_counts = col_counts)
 
   result <- to_string_matrix(result)
   expected <- structure(
@@ -118,4 +122,54 @@ test_that("count_patients_with_event works as expected for different column coun
     .Dim = c(4L, 3L)
   )
   expect_identical(result, expected)
+})
+
+test_that("count_patients_with_flags works as expected", {
+
+  test_data <- tibble(
+    SUBJID = c("1001", "1001", "1001", "1002", "1002", "1002", "1003", "1003", "1003"),
+    ARM = factor(c("A", "A", "A", "A", "A", "A", "B", "B", "B"), levels = c("A", "B")),
+    TRTEMFL = c("Y", "", "", "NA", "", "", "Y", "", ""),
+    AEOUT = c("", "", "", "", "", "", "FATAL", "", "FATAL")
+  )
+  test_data <- test_data %>%
+    mutate(
+      flag1 = TRTEMFL == "Y",
+      flag2 = TRTEMFL == "Y" & AEOUT == "FATAL",
+    ) %>%
+    var_relabel(
+      SUBJID = "A",
+      ARM = "B",
+      TRTEMFL = "C",
+      AEOUT = "D",
+      flag1 = "Total number of patients with at least one adverse event",
+      flag2 = "Total number of patients with fatal AEs"
+    )
+
+  test_adsl_like <- tibble(
+    SUBJID = as.character(1001:1010),
+    ARM = factor(c("A", "A", "B", "B", "A", "A", "A", "B", "B", "A"), levels = c("A", "B")),
+    stringsAsFactors = FALSE
+  )
+  col_counts <- table(test_adsl_like$ARM)
+
+  lyt <- basic_table() %>%
+    split_cols_by("ARM") %>%
+    add_colcounts() %>%
+    count_patients_with_flags(
+      "SUBJID",
+      flag_variables = c("flag1", "flag2"),
+    )
+
+  result <- build_table(lyt, df = test_data, col_counts = col_counts)
+
+  result_matrix <- to_string_matrix(result)
+  expected_matrix <- structure(
+    c("", "", "Total number of patients with at least one adverse event",
+      "Total number of patients with fatal AEs", "A", "(N=6)",
+      "1 (16.67%)", "0 (0%)", "B", "(N=4)", "1 (25%)", "1 (25%)"
+    ),
+    .Dim = c(4L, 3L)
+  )
+  expect_identical(result_matrix, expected_matrix)
 })
