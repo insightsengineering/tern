08a37a33bb2038328ebc14336b7421326030d0ef
Emily de la Rua <edelarua@uwaterloo.ca>
2021-08-12 23:03:53 -0400

    fix bug when "=" used in g_km group labels

--- tests/testthat/test-g_km.R
+++ tests/testthat/test-g_km.R
@@ -35,3 +35,21 @@ test_that("g_km default plot witch ci_ribbon = TRUE works", {
   ))
 
 })
+
+test_that("g_km plot with < = > in group labels works", {
+
+  df <- ex_adtte %>%
+    df_explicit_na() %>%
+    filter(PARAMCD == "OS", ARM == "A: Drug X", BEP01FL == "Y") %>%
+    mutate(is_event = CNSR == 0,
+           group = as.factor(ifelse(AGE>34,">Median","<=Median")))
+
+  variables <- list(tte = "AVAL", is_event = "is_event", arm = "group")
+
+  result <- expect_silent(g_km(
+    df = df,
+    variables = variables,
+    annot_surv_med = FALSE,
+    draw = FALSE
+  ))
+})
