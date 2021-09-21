a470bb0ac6b1d52e4fb53814648b86944092f0d3
Imanol Zubizarreta <87760125+imazubi@users.noreply.github.com>
2021-09-21 17:00:27 +0200

    issue 109: h_stack_by_baskets refinement (#143)
    * base R reshape added
    
    * aag_summary extra argument added
    
    * some updates made
    
    * warning added when there is no interesection between aag and adae levels
    
    * function woorking properly
    
    * unique_id added for making reshape work when keys do not identify records
    
    * some changes made after testing with real stream data
    
    * tests updated
    
    * working for edge cases
    
    * h_stack_by_baskets working nicely even in edge cases
    
    * some minor changes
    
    * Update h_stack_by_baskets.R
    
    * roxygenize
    
    * minor changes
    
    * one correction
    
    * dummy change
    
    * examples updated
    
    * Update h_stack_by_baskets.R
    
    * Âroxygen2 upgraded to 7.1.2 and roxygenize used
    
    * Ârequested changes made
    
    * Update h_stack_by_baskets.R
    
    * roxygenÂ

--- R/h_stack_by_baskets.R
+++ R/h_stack_by_baskets.R
@@ -12,20 +12,63 @@
 #' @param smq_varlabel (`string`)\cr a label for the new variable created.
 #' @param keys (`character`)\cr names of the key variables to be returned
 #' along with the new variable created.
+#' @param aag_summary (`data frame`)\cr data set containing the SMQ baskets
+#' and the levels of interest for the final SMQ variable. This is useful when
+#' there are some levels of interest that are not observed in the `df` dataset.
+#' The two columns of this dataset should be named `basket` and `basket_name`.
 #'
 #' @export
+#'
+#' @importFrom stats reshape
 #'
 #' @examples
 #'
 #' library(scda)
 #'
-#' adae <- synthetic_cdisc_data("rcd_2021_05_05")$adae[1:20 , ]
+#' adae <- synthetic_cdisc_data("latest")$adae[1:20 , ] %>% df_explicit_na()
 #' h_stack_by_baskets(df = adae)
+#'
+#' aag <- data.frame(
+#' NAMVAR = c("CQ01NAM", "CQ02NAM", "SMQ01NAM", "SMQ02NAM"),
+#' REFNAME = c("D.2.1.5.3/A.1.1.1.1 AESI", "D.2.1.5.3/A.2.2.2.2 AESI",
+#'  "C.1.1.1.3/B.2.2.3.1 AESI", "C.1.1.1.3/B.3.3.3.3 AESI"),
+#' SCOPE = c("", "", "BROAD", "BROAD"),
+#' stringsAsFactors = FALSE
+#'  )
+#'
+#' basket_name <- c()
+#' cq_pos <- grep("^(CQ).+NAM$", aag$NAMVAR)
+#' smq_pos <- grep("^(SMQ).+NAM$", aag$NAMVAR)
+#' basket_name[cq_pos] <- aag$REFNAME[cq_pos]
+#' basket_name[smq_pos] <- paste0(
+#' aag$REFNAME[smq_pos], "(", aag$SCOPE[smq_pos], ")"
+#' )
+#'
+#' aag_summary <- data.frame(
+#' basket = aag$NAMVAR,
+#' basket_name = basket_name,
+#' stringsAsFactors = TRUE
+#' )
+#'
+#' result <- h_stack_by_baskets(df = adae, aag_summary = aag_summary)
+#' all(levels(aag_summary$basket_name) %in% levels(result$SMQ))
+#'
+#' h_stack_by_baskets(
+#' df = adae,
+#' aag_summary = NULL,
+#' keys = c("STUDYID", "USUBJID", "AEDECOD", "ARM"),
+#' baskets = "SMQ01NAM"
+#' )
 #'
 h_stack_by_baskets <- function(df,
                                baskets = grep("^(SMQ|CQ).+NAM$", names(df), value = TRUE),
                                smq_varlabel = "Standardized MedDRA Query",
-                               keys = c("STUDYID", "USUBJID", "ASTDTM", "AESEQ", "AETERM")) {
+                               keys = c("STUDYID", "USUBJID", "ASTDTM", "AEDECOD", "AESEQ"),
+                               aag_summary = NULL,
+                               na_level = "<Missing>") {
+
+  #Use of df_explicit_na() in case the user has not previously used
+  df <- df_explicit_na(df, na_level = na_level)
 
   smq_nam <- baskets[startsWith(baskets, "SMQ")]
   # SC corresponding to NAM
@@ -40,28 +83,87 @@ h_stack_by_baskets <- function(df,
     all(endsWith(baskets, "NAM")),
     all(baskets %in% names(df)),
     all(keys %in% names(df)),
-    all(smq_sc %in% names(df))
+    all(smq_sc %in% names(df)),
+    is.string(na_level),
+    sum(baskets %in% names(df)) > 0
   )
 
-  # Concatenate SMQxxxNAM with corresponding SMQxxxSC
-  df_cnct <- df[, c(keys, baskets[startsWith(baskets, "CQ")])]
-
-  for (nam in names(smq)) {
-
-    sc <- smq[nam] # SMQxxxSC corresponding to SMQxxxNAM
-    nam_notna <- !is.na(df[[nam]])
-    new_colname <- paste(nam, sc, sep = "_")
-    df_cnct[nam_notna, new_colname] <- paste0(df[[nam]], "(", df[[sc]], ")")[nam_notna]
-
+  if (!is.null(aag_summary)) {
+    assert_that(
+    is_df_with_variables(
+      df = aag_summary,
+      variables = list(val = c("basket", "basket_name"))
+      )
+    )
+    #Warning in case there is no match between `aag_summary$basket` and `baskets` argument.
+    #Honestly, I think those should completely match. Target baskets should be the same.
+    if (length(intersect(baskets, unique(aag_summary$basket))) == 0) {
+      warning("There are 0 baskets in common between aag_summary$basket and `baskets` argument.")
+    }
   }
 
-  # Transform from wide to long format
-  df_long <- tidyr::pivot_longer(
-    data = df_cnct, cols = -keys, names_to = NULL, values_to = "SMQ", values_drop_na = TRUE
-  )
+  var_labels <- c(var_labels(df[, keys]), smq_varlabel)
 
-  var_labels(df_long[, "SMQ"]) <- smq_varlabel
+  if (all(df[, baskets] == na_level)) { #in case there is no level for the target baskets
+    df_long <- df[-seq_len(nrow(df)), keys] # we just need an empty dataframe keeping all factor levels
+  } else {
 
-  df_long
+    #convert `na_level` records from baskets in NA for the later loop and from wide to long steps
+    df[, c(baskets, smq_sc)][df[, c(baskets, smq_sc)] == na_level] <- NA
 
+    # Concatenate SMQxxxNAM with corresponding SMQxxxSC
+    df_cnct <- df[, c(keys, baskets[startsWith(baskets, "CQ")])]
+
+    for (nam in names(smq)) {
+
+      sc <- smq[nam] # SMQxxxSC corresponding to SMQxxxNAM
+      nam_notna <- !is.na(df[[nam]])
+      new_colname <- paste(nam, sc, sep = "_")
+      df_cnct[nam_notna, new_colname] <- paste0(df[[nam]], "(", df[[sc]], ")")[nam_notna]
+
+    }
+
+      df_cnct$unique_id <- seq(1, nrow(df_cnct))
+      var_cols <- names(df_cnct)[!(names(df_cnct) %in% c(keys, "unique_id"))]
+      # have to convert df_cnct from tibble to dataframe
+      # as it throws a warning otherwise about rownames.
+      # tibble do not support rownames and reshape creates rownames
+
+      df_long <- reshape(
+        data = as.data.frame(df_cnct),
+        varying = var_cols,
+        v.names = "SMQ",
+        idvar = names(df_cnct)[names(df_cnct) %in% c(keys, "unique_id")],
+        direction = "long",
+        new.row.names = seq(prod(length(var_cols), nrow(df_cnct)))
+        )
+
+      df_long <- df_long[!is.na(df_long[, "SMQ"]), !(names(df_long) %in% c("time", "unique_id"))]
+      df_long$SMQ <- as.factor(df_long$SMQ) #nolint
+    }
+
+  smq_levels <- levels(df_long[["SMQ"]])[levels(df_long[["SMQ"]]) != na_level]
+
+  if (!is.null(aag_summary)) {
+    #A warning in case there is no match between ADAE and AAG levels
+    if (length(intersect(smq_levels, unique(aag_summary$basket_name))) == 0) {
+      warning("There are 0 basket levels in common between aag_summary$basket_name and ADAE")
+    }
+    df_long[["SMQ"]] <- factor(
+      df_long[["SMQ"]],
+      levels = sort(
+        c(
+          smq_levels,
+          setdiff(unique(aag_summary$basket_name), smq_levels)
+        )
+      )
+    )
+  } else {
+    df_long[["SMQ"]] <- factor(
+      df_long[["SMQ"]],
+      levels = sort(smq_levels)
+      )
+  }
+  var_labels(df_long) <- var_labels
+  tibble::tibble(df_long)
 }
