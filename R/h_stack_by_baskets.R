d80be8003aedf54381e53221fcc5167dba14c6b1
Jana Stoilova <jana.stoilova@roche.com>
2021-10-05 14:55:35 -0400

    Fill in SMQ levels with baskets not observed in data when aag_summary is not provided.

--- R/h_stack_by_baskets.R
+++ R/h_stack_by_baskets.R
@@ -12,7 +12,7 @@
 #' @param smq_varlabel (`string`)\cr a label for the new variable created.
 #' @param keys (`character`)\cr names of the key variables to be returned
 #' along with the new variable created.
-#' @param aag_summary (`data frame`)\cr data set containing the SMQ baskets
+#' @param aag_summary (`data frame`)\cr containing the SMQ baskets
 #' and the levels of interest for the final SMQ variable. This is useful when
 #' there are some levels of interest that are not observed in the `df` dataset.
 #' The two columns of this dataset should be named `basket` and `basket_name`.
@@ -30,13 +30,13 @@
 #'
 #' aag <- data.frame(
 #' NAMVAR = c("CQ01NAM", "CQ02NAM", "SMQ01NAM", "SMQ02NAM"),
-#' REFNAME = c("D.2.1.5.3/A.1.1.1.1 AESI", "D.2.1.5.3/A.2.2.2.2 AESI",
+#' REFNAME = c("D.2.1.5.3/A.1.1.1.1 AESI", "X.9.9.9.9/Y.8.8.8.8 AESI",
 #'  "C.1.1.1.3/B.2.2.3.1 AESI", "C.1.1.1.3/B.3.3.3.3 AESI"),
 #' SCOPE = c("", "", "BROAD", "BROAD"),
 #' stringsAsFactors = FALSE
 #'  )
 #'
-#' basket_name <- c()
+#' basket_name <- character(nrow(aag))
 #' cq_pos <- grep("^(CQ).+NAM$", aag$NAMVAR)
 #' smq_pos <- grep("^(SMQ).+NAM$", aag$NAMVAR)
 #' basket_name[cq_pos] <- aag$REFNAME[cq_pos]
@@ -104,12 +104,12 @@ h_stack_by_baskets <- function(df,
 
   var_labels <- c(var_labels(df[, keys]), smq_varlabel)
 
-  if (all(df[, baskets] == na_level)) { #in case there is no level for the target baskets
+  #convert `na_level` records from baskets to NA for the later loop and from wide to long steps
+  df[, c(baskets, smq_sc)][df[, c(baskets, smq_sc)] == na_level] <- NA
+
+  if (all(is.na(df[, baskets]))) { #in case there is no level for the target baskets
     df_long <- df[-seq_len(nrow(df)), keys] # we just need an empty dataframe keeping all factor levels
   } else {
-
-    #convert `na_level` records from baskets in NA for the later loop and from wide to long steps
-    df[, c(baskets, smq_sc)][df[, c(baskets, smq_sc)] == na_level] <- NA
 
     # Concatenate SMQxxxNAM with corresponding SMQxxxSC
     df_cnct <- df[, c(keys, baskets[startsWith(baskets, "CQ")])]
@@ -120,7 +120,6 @@ h_stack_by_baskets <- function(df,
       nam_notna <- !is.na(df[[nam]])
       new_colname <- paste(nam, sc, sep = "_")
       df_cnct[nam_notna, new_colname] <- paste0(df[[nam]], "(", df[[sc]], ")")[nam_notna]
-
     }
 
       df_cnct$unique_id <- seq(1, nrow(df_cnct))
@@ -142,12 +141,12 @@ h_stack_by_baskets <- function(df,
       df_long$SMQ <- as.factor(df_long$SMQ) #nolint
     }
 
-  smq_levels <- levels(df_long[["SMQ"]])[levels(df_long[["SMQ"]]) != na_level]
+  smq_levels <- setdiff(levels(df_long[["SMQ"]]), na_level)
 
   if (!is.null(aag_summary)) {
-    #A warning in case there is no match between ADAE and AAG levels
+    #A warning in case there is no match between df and aag_summary records
     if (length(intersect(smq_levels, unique(aag_summary$basket_name))) == 0) {
-      warning("There are 0 basket levels in common between aag_summary$basket_name and ADAE")
+      warning("There are 0 basket levels in common between aag_summary$basket_name and df.")
     }
     df_long[["SMQ"]] <- factor(
       df_long[["SMQ"]],
@@ -159,10 +158,16 @@ h_stack_by_baskets <- function(df,
       )
     )
   } else {
+
+    all_na_basket_flag <- vapply(df[, baskets], function(x){
+      all(is.na(x))
+    }, FUN.VALUE = logical(1))
+    all_na_basket <- baskets[all_na_basket_flag]
+
     df_long[["SMQ"]] <- factor(
       df_long[["SMQ"]],
-      levels = sort(smq_levels)
-      )
+      levels = sort(c(smq_levels, all_na_basket))
+    )
   }
   var_labels(df_long) <- var_labels
   tibble::tibble(df_long)
