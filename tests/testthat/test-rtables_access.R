fbb2dfff152f386a0aba3eca56c7588f6883283d
Jana Stoilova <43623360+anajens@users.noreply.github.com>
2021-10-07 10:45:24 -0400

    Fill in SMQ levels with baskets not observed in data (#202)
    * Fill in SMQ levels with baskets not observed in data when aag_summary is not provided.
    
    * lintr fix
    
    * h_row_counts can handle analysis rows with NULL.
    
    * lintr and indentation fix

--- tests/testthat/test-rtables_access.R
+++ tests/testthat/test-rtables_access.R
@@ -5,6 +5,10 @@ tbl_example <- basic_table() %>%
   summarize_vars("COUNTRY", .stats = "count_fraction") %>%
   build_table(DM)
 
+tbl_with_empty <- rtable(
+  header = c("A: Drug X", "B: Placebo", "C: Combination"),
+  rrow("empty_row", NULL, NULL, NULL)
+)
 
 test_that("h_row_counts works as expected", {
   tab <- tbl_example
@@ -19,6 +23,22 @@ test_that("h_row_counts works as expected", {
   table_row <- collect_leaves(sub_tab)[[1]]
   result <- h_row_counts(table_row, c("B: Placebo", "C: Combination"))
   expected <- c("B: Placebo" = 4, "C: Combination" = 1)
+  expect_identical(result, expected)
+})
+
+test_that("h_row_counts returns NA with empty analysis row", {
+  tab <- tbl_with_empty
+  sub_tab <- tab[3, ]
+  expect_identical(
+    to_string_matrix(sub_tab),
+    structure(
+      c("", "empty_row", "A: Drug X", "", "B: Placebo", "", "C: Combination", ""),
+      .Dim = c(2L, 4L)
+    )
+  )
+  table_row <- collect_leaves(sub_tab)[[1]]
+  result <- h_row_counts(table_row, col_names = c("B: Placebo", "C: Combination"))
+  expected <- c(NA_real_, NA_real_)
   expect_identical(result, expected)
 })
 
