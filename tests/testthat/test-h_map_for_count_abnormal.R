585e3c1e83e7fbc043d8510d441eefd86f6021b9
Rosemary Li <liy396@gene.com>
2021-11-02 12:43:37 -0700

    add unit test for helper function

---
+++ tests/testthat/test-h_map_for_count_abnormal.R
@@ -0,0 +1,31 @@
+library(dplyr)
+df <- data.frame(
+  USUBJID = c(rep("1", 4), rep("2", 4), rep("3", 4)),
+  AVISIT = c(
+    rep("WEEK 1", 2), rep("WEEK 2", 2), rep("WEEK 1", 2), rep("WEEK 2", 2), rep("WEEK 1", 2), rep("WEEK 2", 2)
+  ),
+  PARAM = rep(c("ALT", "CPR"), 6),
+  ANRIND = c("NORMAL", "NORMAL", "LOW", "HIGH", "LOW", "LOW", "HIGH", "HIGH", rep("NORMAL", 4))
+)
+df$ANRIND <- factor(df$ANRIND, levels = c("LOW", "HIGH", "NORMAL")) #nolint
+
+test_that("h_map_for_count_abnormal returns the correct map for default method with healthy input", {
+
+  result <- h_map_for_count_abnormal(
+    df = df,
+    variables = list(anl = "ANRIND", split_rows = "PARAM"),
+    abnormal = list(low = "LOW", high = "HIGH"),
+    method = "default"
+  )
+
+  # because the function doesn't require the order of anl variable, but for unit test stability, we arrange the
+  # order of anl variable within the split_rows group because the order of split_rows is something we want to check
+  result <- result %>% group_by(PARAM) %>% arrange(ANRIND, .by_group = TRUE)
+
+  expected <- data.frame(
+    PARAM = c(rep("ALT", 3), rep("CPR", 3)),
+    ANRIND = rep(c("HIGH", "LOW", "NORMAL"), 2)
+  )
+  expect_identical(as.matrix(result), as.matrix(expected))
+})
+
