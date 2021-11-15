418d2d2b5d85cb39d169d85224892562e685be71
Rosemary Li <liy396@gene.com>
2021-11-15 10:03:47 -0800

    remove production data

--- R/data.R
+++
@@ -1,9 +0,0 @@
-#' Roche’s extended NCI-CTCAE v4 grading criteria
-#'
-#' A metadata defining the NCI-CTCAE v4 laboratory data grading criteria with Roche's interpretation, as proposed by the
-#' Laboratory Analysis Review Board
-#'
-#' @format A data frame with 235 rows and 14 variables
-#'
-#' @source \url{https://pages.github.roche.com/MDIS/stream_doc/2_12/um/vad_concept_grading.html}
-"ext_ncictc4"
