d75162938be4a4508cbce60c8b7ade6b3f8d414d
Imanol Zubizarreta <87760125+imazubi@users.noreply.github.com>
2021-12-06 16:21:07 +0100

    issue 266: create PKPT03 (#271)
    * introduced CV in the s_* function
    
    * created summarize pk
    
    * created the summarize_pok_in_cols function
    
    * improvements made
    
    * made significant steps
    
    * inserted geom mean
    
    * some changes
    
    * Update documentation
    
    * Update tests and NEWS.md
    
    * Fix whitespace error
    
    * Add table test
    
    * Fix SuperLinter error
    
    * Update output table formatting
    
    * Update example data
    
    * Fix indentation
    
    Co-authored-by: Emily de la Rua <edelarua@uwaterloo.ca>
    Co-authored-by: Joe Zhu <sha.joe.zhu@gmail.com>

---
+++ R/summarize_pk.R
@@ -0,0 +1,124 @@
+#' Summary of PK datasets
+#'
+#' These functions can be used to produce summary tables for PK datasets.
+#'
+#' @name summarize_pk
+#'
+NULL
+
+#' @describeIn summarize_pk Statistics function that produces a named list of statistics
+#'   to include as columns in the PK table.
+#' @inheritParams argument_convention
+#' @param custom_label (`string` or `NULL`)\cr if provided and `labelstr` is empty then this will
+#'   be used as the row label.
+#'
+#' @return A named list of 9 statistics:
+#'   - `n`: count of complete sample size for the group.
+#'   - `mean`: group data mean.
+#'   - `sd`: standard deviation of the group data.
+#'   - `cv`: coefficient of variation of the group.
+#'   - `min`: minimum value among the group.
+#'   - `max`: maximum value among the group.
+#'   - `median`: median of the group data.
+#'   - `geom_mean`: geometric mean of the group.
+#'   - `geom_cv`: geometric coefficient of variation of the group.
+#'
+#' @export
+#' @examples
+#'
+#' library(scda)
+#' ADSL <- scda::synthetic_cdisc_data("latest")$adsl
+#' s_summary_pk(ADSL$AGE, custom_label = "stats")
+#'
+s_summary_pk <- function(x,
+                         labelstr = "",
+                         custom_label = NULL,
+                         ...) {
+  row_label <- if (labelstr != "") {
+    labelstr
+  } else if (!is.null(custom_label)) {
+    custom_label
+  } else {
+    "Statistics"
+  }
+  pk_stats <- c("n", "mean", "sd", "cv", "min", "max", "median", "geom_mean", "geom_cv")
+
+  # Calling s_summary.numeric
+  results <- s_summary.numeric(x)
+
+  lapply(results[pk_stats], with_label, row_label)
+}
+
+#' @describeIn summarize_pk Layout creating function which can be used for creating
+#'   summary tables for PK data sets.
+#' @inheritParams argument_convention
+#' @param col_split (`flag`)\cr whether the columns should be split.
+#'
+#' @export
+#' @examples
+#'
+#' lyt <- basic_table() %>%
+#'   split_rows_by(var = "ARM") %>%
+#'   split_rows_by(var = "SEX") %>%
+#'   summarize_pk_in_cols(var = "AGE", col_split = TRUE)
+#'   result <- build_table(lyt, df = ADSL)
+#'   result
+#'
+#' lyt <- basic_table() %>%
+#'   summarize_pk_in_cols(var = "AGE", col_split = TRUE)
+#'   result <- build_table(lyt, df = ADSL)
+#'   result
+#'
+summarize_pk_in_cols <- function(lyt,
+                                 var,
+                                 ...,
+                                 .stats = c("n", "mean", "sd", "cv", "geom_mean", "geom_cv", "median", "min", "max"),
+                                 .labels = c(
+                                 n = "n",
+                                 mean = "Mean",
+                                 sd = "SD",
+                                 cv = "CV % Mean",
+                                 geom_mean = "Geometric Mean",
+                                 geom_cv = "CV % Geometric Mean",
+                                 median = "Median",
+                                 min = "Minimum",
+                                 max = "Maximum"
+                                 ),
+                                 .indent_mods = NULL,
+                                 col_split = TRUE) {
+
+  sum_format <- c(
+    n = "xx.",
+    mean = "xx.xx",
+    sd = "xx.xx",
+    median = "xx.xx",
+    cv = "xx.x",
+    min = "xx.xx",
+    max = "xx.xx",
+    geom_mean = "xx.xx",
+    geom_cv = "xx.x"
+  )
+
+  afun_list <- Map(
+    function(stat) {
+      make_afun(
+        s_summary_pk,
+        .stats = stat,
+        .formats = sum_format[names(sum_format) == stat])
+    },
+    stat = .stats
+  )
+
+  if (col_split) {
+    lyt <- split_cols_by_multivar(
+      lyt = lyt,
+      vars = rep(var, length(.stats)),
+      varlabels = .labels[.stats])
+  }
+
+  summarize_row_groups(
+    lyt = lyt,
+    var = var,
+    cfun = afun_list,
+    extra_args = list(...))
+}
---
+++ tests/testthat/test-table_pkpt03.R
@@ -0,0 +1,31 @@
+# Preparation of the test case.
+library(scda)
+library(dplyr)
+adpc <- synthetic_cdisc_data("rcd_2021_10_13")$adpc
+adpc <- adpc %>% filter(AVAL != 0)
+
+test_that("PKPT03 is produced correctly", {
+
+  l <- basic_table() %>%
+    split_rows_by(
+      var = "ARM",
+      split_fun = keep_split_levels(c("A: Drug X", "C: Combination"))
+    ) %>%
+    split_rows_by(var = "PARAM") %>%
+    summarize_pk_in_cols(var = "AVAL", col_split = TRUE)
+
+  result <- build_table(l, df = adpc)
+
+  result_matrix <- to_string_matrix(result)
+  expected_matrix <- t(structure(
+    c("", "n", "Mean", "SD", "CV % Mean", "Geometric Mean", "CV % Geometric Mean", "Median", "Minimum", "Maximum",
+      "A: Drug X", "", "", "", "", "", "", "", "", "",
+      "Plasma Drug X", "1072", "8.94", "6.26", "70", "3.06", "1687.8", "10.33", "0", "19.86",
+      "Plasma Drug Y", "0", "NA", "NA", "NA", "NaN", "NA", "NA", "NA", "NA",
+      "C: Combination", "", "", "", "", "", "", "", "", "",
+      "Plasma Drug X", "1056", "8.98", "6.37", "70.9", "2.95", "1971.1", "10.44", "0", "19.73",
+      "Plasma Drug Y", "1056", "17.97", "12.74", "70.9", "5.9", "1972.3", "20.88", "0", "39.47"),
+    .Dim = c(10L, 7L)
+  ))
+  expect_identical(result_matrix, expected_matrix)
+})
