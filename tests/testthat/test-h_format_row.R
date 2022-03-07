977ef191a4ab4fd4087044fff05e036bf340b29d
iris <zhaoc26@gene.com>
2022-03-07 19:30:38 +0000

    remove skip_if_fail_rtables_refactor and update the strings for the expected_matrix

--- tests/testthat/test-h_format_row.R
+++ tests/testthat/test-h_format_row.R
@@ -1,6 +1,4 @@
 testthat::test_that("h_format_row returns the correct dataframe", {
-  skip_if_fail_rtables_refactor()
-
   format <- c(mean = "xx.x", mean_ci = "(xx.xx, xx.xx)")
   labels <- c(mean = "My Mean")
 
@@ -8,13 +6,16 @@ testthat::test_that("h_format_row returns the correct dataframe", {
   mean_ci <- c(48, 51)
   x <- list(mean = 50, mean_ci = mean_ci)
   result <- h_format_row(x, format, labels)
-  expected <- data.frame("My Mean" = "50", V1 = "(48, 51)", stringsAsFactors = FALSE, check.names = FALSE)
+  expected <- data.frame("My Mean" = "50.0", V1 = "(48.00, 51.00)", stringsAsFactors = FALSE, check.names = FALSE)
   testthat::expect_identical(result, expected)
 
   # test 2
   attr(mean_ci, "label") <- "Mean 95% CI"
   x <- list(mean = 50, mean_ci = mean_ci)
   result <- h_format_row(x, format, labels)
-  expected <- data.frame("My Mean" = "50", "Mean 95% CI" = "(48, 51)", stringsAsFactors = FALSE, check.names = FALSE)
+  expected <- data.frame(
+    "My Mean" = "50.0", "Mean 95% CI" = "(48.00, 51.00)",
+    stringsAsFactors = FALSE, check.names = FALSE
+  )
   testthat::expect_identical(result, expected)
 })
