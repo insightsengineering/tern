32df741017e01813069a6aaa5c106c0f0bbb700f
brandondbutcher <butcherb@gene.com>
2022-03-08 16:58:57 +0000

    Run styler to close #369.

--- tests/testthat/test-survival_biomarkers_subgroups.R
+++ tests/testthat/test-survival_biomarkers_subgroups.R
@@ -155,7 +155,6 @@ testthat::test_that("extract_survival_biomarkers works as expected with groups_l
 # tabulate_survival_biomarkers ----
 
 testthat::test_that("tabulate_survival_biomarkers works as expected with valid input", {
-
   adtte_f <- adtte %>%
     preprocess_adtte()
 
@@ -227,7 +226,6 @@ testthat::test_that("tabulate_survival_biomarkers functions as expected with NUL
 })
 
 testthat::test_that("tabulate_survival_biomarkers works with only a single biomarker in the data frame", {
-
   df1 <- data.frame(
     biomarker = "BMRKR1",
     biomarker_label = "Continous Level Biomarker 1",
