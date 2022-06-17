c74371fba76dcda4ec86f4ea84b47d860dfeb305
Melkiades <davide.garolini@roche.com>
2022-06-17 16:27:34 +0200

    d_count_missed_doses

--- R/count_missed_doses.R
+++ R/count_missed_doses.R
@@ -21,7 +21,6 @@ s_count_nonmissing <- function(x) {
 }
 
 #' @describeIn count_missed_doses Description function that calculates labels for  [s_count_missed_doses()].
-#' @description `r lifecycle::badge("stable")`
 #' @export
 #' @inheritParams s_count_missed_doses
 #' @return [d_count_missed_doses()] returns a named `character` vector with the labels.
