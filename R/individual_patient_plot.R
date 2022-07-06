fa89c1e95c809720730eadfdddb30a4ae5303e49
Nikolas Burkoff <nikolas.burkoff@roche.com>
2022-07-06 11:09:38 +0100

    use nestcolor in deprecated function

--- R/individual_patient_plot.R
+++ R/individual_patient_plot.R
@@ -42,19 +42,7 @@ h_set_nest_theme <- function(font_size) {
     with = "nestcolor::theme_nest()",
     when = "0.7.9"
   )
-  ggplot2::theme(
-    panel.grid.major = ggplot2::element_blank(),
-    panel.grid.minor = ggplot2::element_blank(),
-    panel.background = ggplot2::element_blank(),
-    panel.border = ggplot2::element_rect(colour = "grey", fill = NA, size = 1),
-    legend.position = "bottom",
-    legend.background = ggplot2::element_blank(),
-    legend.box.background = ggplot2::element_rect(colour = "grey", fill = NA, size = 1),
-    legend.direction = "horizontal",
-    legend.title = ggplot2::element_text(face = "bold"),
-    text = ggplot2::element_text(size = font_size),
-    plot.caption = ggplot2::element_text(hjust = 0)
-  )
+  nestcolor::theme_nest(font_size = font_size)
 }
 
 
