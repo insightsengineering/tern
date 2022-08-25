8f80d2e92b7ccca16e8159044a86d82ccad100ce
arkadiuszbeer <86738093+arkadiuszbeer@users.noreply.github.com>
2022-08-25 11:33:45 +0200

    no req. for xml2

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,8 +4,7 @@ if (requireNamespace("testthat", quietly = TRUE)) {
   is_on_ci <- isTRUE(as.logical(Sys.getenv("CI")))
   if (is_on_ci) {
     reporter <- MultiReporter$new(list(
-      CheckReporter$new(),
-      JunitReporter$new(file = "junit-result.xml")
+      CheckReporter$new()
     ))
     test_results <- test_check(pkg_name, reporter = reporter)
     saveRDS(test_results, "unit_testing_results.rds")
