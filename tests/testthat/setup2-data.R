52ae66c50a4ac94653cafba13b0c829fa67b0d6b
Melkiades <davide.garolini@roche.com>
2022-09-13 19:56:51 +0200

    reducing dependencies from DM and formatters

--- tests/testthat/setup2-data.R
+++ tests/testthat/setup2-data.R
@@ -9,6 +9,12 @@ adpp_raw <- test_data$adpp
 adrs_raw <- test_data_recent$adrs
 adtte_raw <- test_data_recent$adtte
 
+# Data as DM from `formatters`
+adsl_dm <- adsl_raw %>%
+  select(c("AGE", "SEX", "RACE", "COUNTRY", "ARM", "BMRKR1", "STRATA1")) %>%
+  mutate(ID = paste0("S", seq_len(nrow(adsl_raw))))
+adsl_dm <- var_relabel(adsl_dm, ID = "subject id")
+
 # Bladder data from survival (previously function `get_bladder`)
 dta_bladder_raw <- local({
   # Setting general random for data generation
--- tests/testthat/test-prune_occurrences.R
+++ tests/testthat/test-prune_occurrences.R
@@ -1,16 +1,11 @@
-# Data as DM from `formatters`
-adsl_local <- adsl_raw %>%
-  select(c("AGE", "SEX", "RACE", "COUNTRY", "ARM", "BMRKR1", "STRATA1")) %>%
-  mutate(ID = paste0("S", seq_len(nrow(adsl_raw))))
-adsl_local <- var_relabel(adsl_local, ID = "subject id")
-
+# Pre-processing the table
 tab <- basic_table() %>%
   split_cols_by("ARM") %>%
   split_rows_by("RACE") %>%
   split_rows_by("STRATA1") %>%
   summarize_row_groups() %>%
   summarize_vars("COUNTRY", .stats = "count_fraction") %>%
-  build_table(adsl_local)
+  build_table(adsl_dm)
 
 testthat::test_that("keep_rows works in a special case identical to standard pruning", {
   row_condition <- !CombinationFunction(all_zero_or_na)
