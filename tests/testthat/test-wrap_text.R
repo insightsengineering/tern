a3328c784adbf05b85e979eadda3de014f938f83
Melkiades <davide.garolini@roche.com>
2022-11-10 11:56:35 +0100

    fix new deprecated warning

--- tests/testthat/test-wrap_text.R
+++ tests/testthat/test-wrap_text.R
@@ -1,6 +1,8 @@
 testthat::test_that("wrap_text works with default settings", {
   text <- "This is a test with many words and more"
-  result <- wrap_text(txt = text, width = grid::unit(4, "cm"), collapse = "\n")
+  result <- testthat::expect_warning(
+    wrap_text(txt = text, width = grid::unit(4, "cm"), collapse = "\n")
+  )
   expected <- "This is a test with\nmany words and\nmore"
   testthat::expect_identical(result, expected)
 })
