eb7e657e0aff42457828a164cbdd16bb98ab86e1
Melkiades <davide.garolini@roche.com>
2022-11-30 17:28:17 +0100

    adding direct test of single new features

--- tests/testthat/test-table_pkpct01.R
+++ tests/testthat/test-table_pkpct01.R
@@ -55,3 +55,104 @@ testthat::test_that("PKCT01 is produced correctly", {
 
   testthat::expect_identical(result_matrix, expected_matrix)
 })
+
+testthat::test_that("Specific PKCT01 features are present", {
+  # Setting up the data
+  adpc_1 <- adpc %>%
+    mutate(
+      NRELTM1 = as.factor(NRELTM1),
+      AVALC = as.factor(AVALC)
+    ) %>%
+    filter(ACTARM %in% c("A: Drug X")) %>%
+    mutate(ACTARM = factor(ACTARM, levels = c("A: Drug X"))) %>%
+    select(NRELTM1, ACTARM, VISIT, AVAL, PARAM, AVALC)
+  adpc_2 <- adpc_1 %>% mutate(AVAL = AVALC)
+
+  # Row structure
+  l_rows <- basic_table() %>%
+    split_rows_by(
+      var = "ACTARM",
+      split_label = "Cohort/Treatment",
+      label_pos = "topleft"
+    ) %>%
+    split_rows_by(
+      var = "VISIT",
+      split_label = "Visit",
+      label_pos = "topleft"
+    ) %>%
+    split_rows_by(
+      var = "NRELTM1",
+      split_label = "Norminal Time from First Dose",
+      label_pos = "topleft"
+    )
+
+  # Column results for numeric values
+  l1 <- l_rows %>%
+    summarize_vars_in_cols(
+      var = "AVAL",
+      col_split = TRUE,
+      .stats = c("n", "mean", "sd", "cv", "geom_mean", "geom_cv", "median", "min", "max"),
+      .formats = c(
+        n = "xx.",
+        mean = sprintf_format("%.3e"),
+        sd = sprintf_format("%.3e"),
+        cv = "xx.x",
+        median = sprintf_format("%.3e"),
+        geom_mean = sprintf_format("%.3e"),
+        geom_cv = "xx.x",
+        min = sprintf_format("%.3e"),
+        max = sprintf_format("%.3e")
+      ),
+      .labels = c(
+        n = "n",
+        mean = "Mean",
+        sd = "SD",
+        cv = "CV (%) Mean",
+        geom_mean = "Geometric Mean",
+        geom_cv = "CV % Geometric Mean",
+        median = "Median",
+        min = "Minimum",
+        max = "Maximum"
+      ),
+      na_str = "NE"
+    )
+  result1 <- build_table(l1, df = adpc_1)
+
+  # Column results for character counts
+  l2 <- l_rows %>%
+    summarize_vars_in_cols(
+      var = "AVAL",
+      var_type = "character",
+      col_split = TRUE,
+      .stats = c("n_blq"),
+      .labels = c(
+        n_blq = "Number\nof\n<LTR/BLQ>s"
+      )
+    )
+  result2 <- build_table(l2, df = adpc_2)
+
+  # Merging and trimming
+  result <- cbind_rtables(result1[, 1], result2, result1[, 2:ncol(result1)]) %>%
+    trim_rows()
+
+  # Decorating
+  main_title(result) <- "Summary of PK Concentrations by Nominal Time and Treatment: PK Evaluable\n Protocol: xxxxx"
+  subtitles(result) <- paste("Analyte: ", unique(unique(adpc$PARAM)), "Treatment:", unique(unique(adpc$ACTARM)))
+  main_footer(result) <- "NE: Not Estimable"
+
+  # Comparing
+  string_res <- strsplit(toString(matrix_form(result, TRUE)), "\n")
+
+  # Checking NAs are NEs
+  testthat::expect_false(any(sapply(string_res, grepl, pattern = "NA")))
+  testthat::expect_equal(sum(sapply(string_res, grepl, pattern = "NE")), 4L)
+
+  # Checking significative digits (DISCLAIMER: this is an hack and is NOT well supported)
+  mean_vals <- rtables::cell_values(result,
+    rowpath = NULL,
+    colpath = c("multivars", "AVAL._[[2]]_.")
+  ) %>%
+    unlist()
+  names(mean_vals) <- NULL
+  # testthat::expect_equal(mean_vals, signif(mean_vals, 3))
+})
