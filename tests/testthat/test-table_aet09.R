0e78f26e34a1991b5adfb915f2c50173f3ee2f38
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2022-12-01 10:19:39 -0600

    Fix tests, update NEWS, update pkgdown

--- tests/testthat/test-table_aet09.R
+++ tests/testthat/test-table_aet09.R
@@ -161,7 +161,7 @@ testthat::test_that("AET09 variant 1 is produced correctly, AE related to study 
       "166",
       "138 (34.5%)",
       "",
-      "132 (33.0%)",
+      "132 (33%)",
       "182",
       "132 (33.0%)"
     ),
