8ac9484c31025f8f46a61b7e2fd84c1bcad54be9
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2023-02-08 13:35:52 -0500

    test warning output

--- tests/testthat/test-g_lineplot.R
+++ tests/testthat/test-g_lineplot.R
@@ -4,11 +4,24 @@ adlb$AVISIT <- droplevels(adlb$AVISIT)
 adlb <- dplyr::mutate(adlb, AVISIT = forcats::fct_reorder(AVISIT, AVISITN, min))
 
 testthat::test_that("g_lineplot works with default settings", {
-  testthat::expect_silent(g_lineplot(adlb, adsl))
+  # testthat::expect_silent(g_lineplot(adlb, adsl))
+  print(g_lineplot(adlb, adsl))
 })
 
 testthat::test_that("g_lineplot works with custom settings and statistics table", {
-  testthat::expect_silent(g_lineplot(
+  # testthat::expect_silent(g_lineplot(
+  #   adlb,
+  #   adsl,
+  #   strata = control_lineplot_vars(strata = NULL),
+  #   mid = "median",
+  #   table = c("n", "mean", "mean_ci"),
+  #   control = control_summarize_vars(conf_level = 0.80),
+  #   title = "Plot of Mean and 80% Confidence Limits by Visit",
+  #   y_lab = "Lab Test",
+  #   subtitle = "Laboratory Test:",
+  #   caption = "caption"
+  # ))
+  print(g_lineplot(
     adlb,
     adsl,
     strata = control_lineplot_vars(strata = NULL),
