3ee3121106fa6dd40e8319f05df64adf8beb3f5b
Melkiades <davide.garolini@roche.com>
2023-02-14 09:17:33 +0100

    starting deprecation cycle for footnotes and add_footnotes

--- R/footnotes.R
+++ R/footnotes.R
@@ -1,6 +1,6 @@
 #' Assign Footnotes
 #'
-#' @description `r lifecycle::badge("stable")`
+#' @description `r lifecycle::badge("deprecated")`
 #'
 #' Assign value to attribute footnote of object `x`.
 #'
@@ -12,6 +12,7 @@
 #' footnotes(x) <- "Species are equally distributed"
 #' attributes(x)
 `footnotes<-` <- function(x, value = NULL) { # nolint
+  lifecycle::deprecate_warn("0.7.10", "footnotes()", "rtables::main_footer()")
   attr(x, "footnote") <- value
   x
 }
@@ -19,7 +20,7 @@
 
 #' Retrieve Footnotes
 #'
-#' @description `r lifecycle::badge("stable")`
+#' @description `r lifecycle::badge("deprecated")`
 #'
 #' Retrieve value from attribute `footnote` of object `x`.
 #'
@@ -30,12 +31,13 @@
 #' footnotes(x) <- "Species are equally distributed"
 #' footnotes(x)
 footnotes <- function(x) {
+  lifecycle::deprecate_warn("0.7.10", "footnotes()", "rtables::main_footer()")
   attr(x, "footnote")
 }
 
 #' Add Footnotes
 #'
-#' @description `r lifecycle::badge("stable")`
+#' @description `r lifecycle::badge("deprecated")`
 #'
 #' This adds more footnotes.
 #'
@@ -49,6 +51,7 @@ footnotes <- function(x) {
 #' add_footnotes(x) <- "Add more footnotes"
 #' footnotes(x)
 `add_footnotes<-` <- function(x, value) { # nolint
+  lifecycle::deprecate_warn("0.7.10", "add_footnotes()", "rtables::main_footer()")
   footnotes(x) <- c(footnotes(x), value)
   x
 }
