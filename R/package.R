e8236129df321b798981f71df7f365652477f024
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2023-05-11 17:07:30 -0400

    Remove version requirement for missing global definitions

--- R/package.R
+++ R/package.R
@@ -16,15 +16,13 @@
 #' @importFrom stats pchisq
 NULL
 
-# We need this to avoid R CMD check warning about missing global definitions.
-if (getRversion() >= "2.15.1") {
-  utils::globalVariables(c(
-    ".",
-    "x",
-    "control_coxph",
-    "control_incidence_rate",
-    "control_summarize_vars",
-    "control_surv_time",
-    "control_surv_timepoint"
-  ))
-}
+# Resolve missing global definitions:
+utils::globalVariables(c(
+  ".",
+  "x",
+  "control_coxph",
+  "control_incidence_rate",
+  "control_summarize_vars",
+  "control_surv_time",
+  "control_surv_timepoint"
+))
