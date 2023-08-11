e68ab588388e80f0eda9de6bf10edbf80bed9e91
Melkiades <davide.garolini@roche.com>
2023-08-11 17:38:14 +0200

    adding back some vars and roxy

--- R/argument_convention.R
+++ R/argument_convention.R
@@ -29,13 +29,19 @@
 #' @param draw (`flag`)\cr whether the plot should be drawn.
 #' @param drop (`flag`)\cr should non appearing occurrence levels be dropped from the resulting table.
 #'   Note that in that case the remaining occurrence levels in the table are sorted alphabetically.
+#' @param groups_lists (named `list` of `list`)\cr optionally contains for each `subgroups` variable a
+#'   list, which specifies the new group levels via the names and the
+#'   levels that belong to it in the character vectors that are elements of the list.
 #' @param id (`string`)\cr subject variable name.
 #' @param is_event (`logical`)\cr `TRUE` if event, `FALSE` if time to event is censored.
 #' @param indent_mod `r lifecycle::badge("deprecated")` Please use the `.indent_mods` argument instead.
+#' @param label_all (`string`)\cr label for the total population analysis.
 #' @param labelstr (`character`)\cr label of the level of the parent split currently being summarized
 #'   (must be present as second argument in Content Row Functions). See [rtables::summarize_row_groups()]
 #'   for more information.
 #' @param lyt (`layout`)\cr input layout where analyses will be added to.
+#' @param method (`string`)\cr specifies the test used to calculate the p-value for the difference between
+#'   two proportions. For options, see [s_test_proportion_diff()]. Default is `NULL` so no test is performed.
 #' @param na.rm (`flag`)\cr whether `NA` values should be removed from `x` prior to analysis.
 #' @param na_level (`string`)\cr string used to replace all `NA` or empty values in the output.
 #' @param nested (`flag`)\cr whether this layout instruction should be applied within the existing layout structure _if
@@ -56,7 +62,7 @@
 #' @param vars (`character`)\cr variable names for the primary analysis variable to be iterated over.
 #' @param var (`string`)\cr single variable name for the primary analysis variable.
 #' @param x (`numeric`)\cr vector of numbers we want to analyze.
-#'
+
 #' @details Although this function just returns `NULL` it has two uses, for
 #'   the `tern` users it provides a documentation of arguments that are
 #'   commonly and consistently used in the framework. For the developer it adds a
