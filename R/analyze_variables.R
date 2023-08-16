4e045710e6de84adc622d746e682758b9ec75886
Melkiades <davide.garolini@roche.com>
2023-08-16 18:03:40 +0200

    fix deprecation

--- R/analyze_variables.R
+++ R/analyze_variables.R
@@ -4,6 +4,9 @@
 #'
 #' We use the S3 generic function [s_summary()] to implement summaries for different `x` objects. This
 #' is used as a statistics function in combination with the analyze function [analyze_vars()].
+#' Deprecation cycle started for `summarize_vars` as it is going to renamed into
+#' `analyze_vars`. Intention is to reflect better the core underlying `rtables`
+#' functions; in this case [rtables::analyze()].
 #'
 #' @inheritParams argument_convention
 #'
@@ -624,9 +627,6 @@ create_afun_summary <- function(.stats, .formats, .labels, .indent_mods) {
 #'   should be a name-value pair with name corresponding to a statistic specified in `.stats` and value the indentation
 #'   for that statistic's row label.
 #'
-#' @note Deprecation cycle started for `summarize_vars` as it is going to renamed into
-#'   `analyze_vars`. Intention is to reflect better the core underlying `rtables`
-#'   functions; in this case [rtables::analyze()].
 #'
 #' @return
 #' * `analyze_vars()` returns a layout object suitable for passing to further layouting functions,
@@ -716,5 +716,34 @@ analyze_vars <- function(lyt,
     section_div = section_div
   )
 }
-
-summarize_vars <- analyze_vars
+#' @inherit analyze_vars
+summarize_vars <- function(lyt,
+                           vars,
+                           var_labels = vars,
+                           nested = TRUE,
+                           ...,
+                           na.rm = TRUE, # nolint
+                           na_level = NA_character_,
+                           show_labels = "default",
+                           table_names = vars,
+                           section_div = NA_character_,
+                           .stats = c("n", "mean_sd", "median", "range", "count_fraction"),
+                           .formats = NULL,
+                           .labels = NULL,
+                           .indent_mods = NULL) {
+  lifecycle::deprecate_warn(when = "0.8.5.9010", "summarize_vars", "analyze_vars")
+  analyze_vars(lyt,
+               vars,
+               var_labels,
+               nested,
+               ...,
+               na.rm, # nolint
+               na_level,
+               show_labels,
+               table_names,
+               section_div,
+               .stats,
+               .formats,
+               .labels,
+               .indent_mods)
+}
