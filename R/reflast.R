471ede68b8a0b4840c486bd7008102fdbbcbd446
stoilovs <jana.stoilova@roche.com>
2023-10-15 18:06:09 -0400

    clean up docs

--- R/reflast.R
+++ R/reflast.R
@@ -1,62 +1,65 @@
-#' Split Function to Place Reference Group Last
+#' Split Function to Place Reference Group Facet Last
 #'
-#' @param splret
-#' @param spl
-#' @param fulldf
-#' @param ...
+#' Place reference group facet last during post-processing stage in a custom split fun.
 #'
-#' @return
+#' @inheritParams rtables::custom_split_funs
+#' @param splret result of the core split
+#' @param fulldf data.frame of incoming data to be split
+#'
 #' @export
 #'
+#' @seealso [rtables::make_split_fun()]
+#'
 #' @examples
-#' dat2 <- data.frame(
-#' x = factor(letters[1:5], levels = letters[5:1]),
-#' y = 1:5
+#' library(dplyr)
+#'
+#' dat <- data.frame(
+#'   x = factor(letters[1:5], levels = letters[5:1]),
+#'   y = 1:5
 #' )
 #'
+#' # With rtables layout functions
 #' basic_table() %>%
-#'   split_cols_by("x", ref_group = "a", split_fun = make_split_fun(post = list(ref_last))) %>%
+#'   split_cols_by("x", ref_group = "c", split_fun = make_split_fun(post = list(ref_group_last))) %>%
 #'   analyze("y") %>%
-#'   build_table(dat2)
+#'   build_table(dat)
+#'
+#' # With tern layout funcitons
+#' adtte_f <- tern_ex_adtte %>%
+#'   filter(PARAMCD == "OS") %>%
+#'   mutate(
+#'     AVAL = day2month(AVAL),
+#'     is_event = CNSR == 0
+#'   )
 #'
 #' basic_table() %>%
-#'   split_cols_by("x", ref_group = "c", split_fun = make_split_fun(post = list(ref_last))) %>%
-#'   analyze("y") %>%
-#'   build_table(dat2)
+#'   split_cols_by(var = "ARMCD", ref_group = "ARM B", split_fun = make_split_fun(post = list(ref_group_last))) %>%
+#'   add_colcounts() %>%
+#'   surv_time(
+#'     vars = "AVAL",
+#'     var_labels = "Survival Time (Months)",
+#'     is_event = "is_event",
+#'   ) %>%
+#' build_table(df = adtte_f)
 #'
-#' basic_table() %>%
-#'   split_cols_by("x", ref_group = "e", split_fun = make_split_fun(post = list(ref_last))) %>%
-#'   analyze("y") %>%
-#'   build_table(dat2)
-#'
-ref_last <- function(splret, spl, fulldf, ...) {
+ref_group_last <- function(splret, spl, fulldf){
+
+  if(!"ref_group_value" %in% slotNames(spl)) {
+    stop("Reference group is undefined.")
+  }
 
   spl_var <- spl@payload
   init_lvls <- levels(fulldf[[spl_var]])
+
+  if(!all(names(splret$values) %in% init_lvls)) {
+    stop("This split function does not work with combination facets.")
+  }
+
   ref_group_pos <- which(init_lvls == spl@ref_group_value)
   reord_lvls <- c(init_lvls[-ref_group_pos], init_lvls[ref_group_pos])
-
-  if (identical(names(splret$values), init_lvls)) {
-
-    ord <- match(reord_lvls, init_lvls)
-
-  } else {
-
-    ord <- match(reord_lvls, names(splret$values))
-  }
+  ord <- match(reord_lvls, names(splret$values))
 
   make_split_result(splret$values[ord],
                     splret$datasplit[ord],
                     splret$labels[ord])
 }
-
-# THis isn't quite working yet.
-# sf <- make_split_fun(
-#   post = list(add_combo_facet("A_C", "Arms A+C", c("A: Drug X", "C: Combination")),
-#               ref_last)
-# )
-#
-# basic_table(show_colcounts = TRUE) %>%
-#   split_cols_by("ARM", split_fun = sf, ref_group = "B: Placebo") %>%
-#   analyze("AGE") %>%
-#   build_table(DM)
