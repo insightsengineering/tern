6378412b39a2c962596daced74c144f8847b50d1
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2023-11-21 14:52:43 -0500

    Clean up code

--- R/utils_ggplot.R
+++ R/utils_ggplot.R
@@ -36,17 +36,19 @@
 #' @export
 rtable2gg <- function(tbl, fontsize = 4, colwidths = NULL, first_col_padding = 0) {
   mat <- matrix_form(tbl)
-  mfstrings <- mat$strings
-  mfindent <- mat$row_info$indent
-  nlh <- mf_nlheader(mat)
-  shared_hdr_rows <- which(apply(mat$display, 1, function(x) (any(!x))))
+  mat_strings <- mf_strings(mat)
+  mat_aligns <- mf_aligns(mat)
+  mat_indent <- mf_rinfo(mat)$indent
+  nlines_hdr <- mf_nlheader(mat)
+  shared_hdr_rows <- which(apply(mf_display(mat), 1, function(x) (any(!x))))
 
-  tbl_df <- data.frame(mfstrings)
-  body_rows <- seq(nlh + 1, nrow(tbl_df))
+  tbl_df <- data.frame(mat_strings)
+  body_rows <- seq(nlines_hdr + 1, nrow(tbl_df))
+  mat_aligns <- apply(mat_aligns, 1:2, function(x) if (x == "left") 0 else if (x == "center") 0.5 else 1)
 
   # Apply indentation in first column
   tbl_df[body_rows, 1] <- sapply(body_rows, function(i) {
-    ind_i <- mfindent[i - nlh] * 4
+    ind_i <- mat_indent[i - nlines_hdr] * 4
     if (ind_i > 0) paste0(paste(rep(" ", ind_i), collapse = ""), tbl_df[i, 1]) else tbl_df[i, 1]
   })
 
@@ -56,22 +58,28 @@ rtable2gg <- function(tbl, fontsize = 4, colwidths = NULL, first_col_padding = 0
   }
   tot_width <- sum(colwidths) + first_col_padding
 
-  if (length(shared_hdr_rows) > 0) tbl_df <- tbl_df[-shared_hdr_rows, ]
+  if (length(shared_hdr_rows) > 0) {
+    tbl_df <- tbl_df[-shared_hdr_rows, ]
+    mat_aligns <- mat_aligns[-shared_hdr_rows, ]
+  }
 
   res <- ggplot(data = tbl_df) +
     theme_void() +
     scale_x_continuous(limits = c(0, tot_width)) +
-    scale_y_continuous(limits = c(0, nrow(mfstrings))) +
-    geom_segment(aes(x = 0, xend = tot_width, y = nrow(mfstrings) - nlh + 0.5, yend = nrow(mfstrings) - nlh + 0.5))
+    scale_y_continuous(limits = c(0, nrow(mat_strings))) +
+    geom_segment(aes(
+      x = 0, xend = tot_width,
+      y = nrow(mat_strings) - nlines_hdr + 0.5, yend = nrow(mat_strings) - nlines_hdr + 0.5
+    ))
 
   # If header content spans multiple columns, center over these columns
   if (length(shared_hdr_rows) > 0) {
-    mfstrings[shared_hdr_rows, ] <- trimws(mfstrings[shared_hdr_rows, ])
+    mat_strings[shared_hdr_rows, ] <- trimws(mat_strings[shared_hdr_rows, ])
     for (hr in shared_hdr_rows) {
-      hdr_lbls <- unique(mfstrings[hr, ])
+      hdr_lbls <- unique(mat_strings[hr, ])
       hdr_lbls <- hdr_lbls[nzchar(hdr_lbls)]
       for (hl in hdr_lbls) {
-        which_cols <- which(mfstrings[hr, ] == hl)
+        which_cols <- which(mat_strings[hr, ] == hl)
         line_pos <- c(
           sum(colwidths[1:(which_cols[1] - 1)]) + 1 + first_col_padding,
           sum(colwidths[1:max(which_cols)]) - 1 + first_col_padding
@@ -80,15 +88,15 @@ rtable2gg <- function(tbl, fontsize = 4, colwidths = NULL, first_col_padding = 0
         res <- res +
           geom_text(
             x = lbl_pos,
-            y = nrow(mfstrings) + 1 - hr,
+            y = nrow(mat_strings) + 1 - hr,
             label = hl,
             size = fontsize
           ) +
           geom_segment(
             x = line_pos[1],
             xend = line_pos[2],
-            y = nrow(mfstrings) - hr + 0.5,
-            yend = nrow(mfstrings) - hr + 0.5
+            y = nrow(mat_strings) - hr + 0.5,
+            yend = nrow(mat_strings) - hr + 0.5
           )
       }
     }
@@ -100,7 +108,7 @@ rtable2gg <- function(tbl, fontsize = 4, colwidths = NULL, first_col_padding = 0
       x = if (i == 1) 0 else sum(colwidths[1:i]) - 0.5 * colwidths[i] + first_col_padding,
       y = rev(seq_len(nrow(tbl_df))),
       label = tbl_df[, i],
-      hjust = if (i == 1) 0 else 0.5,
+      hjust = mat_aligns[, i],
       size = fontsize
     )
   }
