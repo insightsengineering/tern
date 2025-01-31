17d84c50f26fbcd9c4f8ea5a8dd6445a3c04c079
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2025-01-31 10:55:25 -0500

    Simplify tests

--- tests/testthat/test-survival_duration_subgroups.R
+++ tests/testthat/test-survival_duration_subgroups.R
@@ -288,16 +288,15 @@ testthat::test_that("tabulate_survival_subgroups riskdiff argument works as expe
   testthat::expect_snapshot(res)
 
   # pct works
-  result <- basic_table() %>%
+  result2 <- basic_table() %>%
     tabulate_survival_subgroups(
       df,
       time_unit = adtte$AVALU[1],
-      riskdiff = control_riskdiff(
-        col_label = "Prop. Diff\n(95% CI)",
-        pct = FALSE
-      )
+      riskdiff = control_riskdiff(pct = FALSE)
     )
 
-  res <- testthat::expect_silent(result)
-  testthat::expect_snapshot(res)
+  testthat::expect_equal(
+    cell_values(result2)[[1]][[8]],
+    cell_values(result)[[1]][[8]] / 100
+  )
 })
