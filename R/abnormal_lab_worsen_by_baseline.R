1a3841f68446273ec91572a0f41fee5d3d106a52
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2025-02-25 14:28:22 -0500

    Not using custom_stat_functions

--- R/abnormal_lab_worsen_by_baseline.R
+++ R/abnormal_lab_worsen_by_baseline.R
@@ -88,10 +88,15 @@ a_count_abnormal_lab_worsen_by_baseline <- function(df,
   extra_afun_params <- retrieve_extra_afun_params(names(dots_extra_args$.additional_fun_parameters))
   dots_extra_args$.additional_fun_parameters <- NULL
 
+  # Check for user-defined functions
+  default_and_custom_stats_list <- .split_std_from_custom_stats(.stats)
+  .stats <- default_and_custom_stats_list$all_stats
+  custom_stat_functions <- default_and_custom_stats_list$custom_stats
+
   # Apply statistics function
   x_stats <- .apply_stat_functions(
     default_stat_fnc = s_count_abnormal_lab_worsen_by_baseline,
-    custom_stat_fnc_list = NULL,
+    custom_stat_fnc_list = custom_stat_functions,
     args_list = c(
       df = list(df),
       extra_afun_params,
@@ -100,7 +105,11 @@ a_count_abnormal_lab_worsen_by_baseline <- function(df,
   )
 
   # Fill in formatting defaults
-  .stats <- get_stats("abnormal_lab_worsen_by_baseline", stats_in = .stats)
+  .stats <- get_stats(
+    "abnormal_lab_worsen_by_baseline",
+    stats_in = .stats,
+    custom_stats_in = names(custom_stat_functions)
+  )
   levels_per_stats <- lapply(x_stats, names)
   .formats <- get_formats_from_stats(.stats, .formats, levels_per_stats)
   .labels <- get_labels_from_stats(.stats, .labels, levels_per_stats)
